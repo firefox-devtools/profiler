0050a9f6bc744e58c997469cb585435d8905c002
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-08-26 18:27:31 -0400

    Add tree parsing code

--- parser.js
+++ parser.js
@@ -3,6 +3,48 @@ function Sample(name, extraInfo) {
   this.extraInfo = extraInfo;
 }
 
+function TreeNode(name, parent) {
+  this.name = name;
+  this.children = [];
+  this.counter = 1;
+  this.parent = parent;
+  this.depth = 0;
+  var node = this.parent;
+  while (node) {
+    this.depth++;
+    node = node.parent;
+  }
+}
+TreeNode.prototype.traverse = function TreeNode_traverse(callback) {
+  if (this.children.length == 0) {
+    return false;
+  }
+  for (var i = 0; i < this.children.length; ++i) {
+    var child = this.children[i];
+    var result = callback(child);
+    if (result !== false) {
+      return result;
+    }
+  }
+  return false;
+};
+TreeNode.prototype.followPath = function TreeNode_followPath(path) {
+  var node = this;
+nextIteration:
+  for (var i = 0; i < path.length; ++i) {
+    var segment = path[i];
+    for (var j = 0; j < node.children.length; ++j) {
+      var child = node.children[j];
+      if (child.name == segment) {
+        node = child;
+        continue nextIteration;
+      }
+    }
+    break;
+  }
+  return node;
+};
+
 function Parser() {}
 Parser.prototype = {
   parse: function Parser_parse(data) {
@@ -38,5 +80,47 @@ Parser.prototype = {
       }
     }
     return samples;
+  },
+
+  parseCallStack: function Parser_parseCallStack(serialization) {
+    return serialization.split(",");
+  },
+
+  convertToCallTree: function Parser_convertToCallTree(samples) {
+    var treeRoot = null;
+    for (var i = 0; i < samples.length; ++i) {
+      var sample = samples[i];
+      var callstack = this.parseCallStack(sample.name);
+      if (!treeRoot) {
+        treeRoot = new TreeNode(callstack[0], null);
+      } else {
+        var newChild = treeRoot.followPath(callstack.slice(1));
+        if (newChild.name == callstack[callstack.length - 1]) {
+          // we found the exact node, so let's just increment
+          // the counters through the parent chain.
+          var node = newChild;
+          while (node) {
+            node.counter++;
+            node = node.parent;
+          }
+        } else {
+          var depth = newChild.depth + 1;
+          var remainingCallstack = callstack.slice(depth);
+          var node = newChild;
+          while (node) {
+            node.counter++;
+            node = node.parent;
+          }
+          node = newChild;
+          for (var j = 0; j < remainingCallstack.length; ++j) {
+            var frame = remainingCallstack[j];
+            var child = new TreeNode(frame, node);
+            node.children.push(child);
+            node = child;
+          }
+        }
+      }
+    }
+    return treeRoot;
   }
 };
--- ui.js
+++ ui.js
@@ -1,5 +1,6 @@
 function parse() {
   var parser = new Parser();
   var data = parser.parse(document.getElementById("data").value);
+  data = parser.convertToCallTree(data);
   document.getElementById("result").textContent = data.toSource();
 }
