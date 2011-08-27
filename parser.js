8ff9b0b30b4235fae00e4a52662586d3f24dfd88
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-08-27 15:13:25 -0400

    Commit the github pages branch to match 2651b7b601c3abc62f46944f8a3d225285daa769

---
+++ parser.js
@@ -0,0 +1,126 @@
+function Sample(name, extraInfo) {
+  this.name = name;
+  this.extraInfo = extraInfo;
+}
+
+function TreeNode(name, parent) {
+  this.name = name;
+  this.children = [];
+  this.counter = 1;
+  this.parent = parent;
+  this.depth = 0;
+  var node = this.parent;
+  while (node) {
+    this.depth++;
+    node = node.parent;
+  }
+}
+TreeNode.prototype.traverse = function TreeNode_traverse(callback) {
+  if (this.children.length == 0) {
+    return false;
+  }
+  for (var i = 0; i < this.children.length; ++i) {
+    var child = this.children[i];
+    var result = callback(child);
+    if (result !== false) {
+      return result;
+    }
+  }
+  return false;
+};
+TreeNode.prototype.followPath = function TreeNode_followPath(path) {
+  var node = this;
+nextIteration:
+  for (var i = 0; i < path.length; ++i) {
+    var segment = path[i];
+    for (var j = 0; j < node.children.length; ++j) {
+      var child = node.children[j];
+      if (child.name == segment) {
+        node = child;
+        continue nextIteration;
+      }
+    }
+    break;
+  }
+  return node;
+};
+
+function Parser() {}
+Parser.prototype = {
+  parse: function Parser_parse(data) {
+    var lines = data.split("\n");
+    var extraInfo = {};
+    var samples = [];
+    for (var i = 0; i < lines.length; ++i) {
+      var line = lines[i];
+      if (line.length < 2 || line[1] != '-') {
+        // invalid line, ignore it
+        continue;
+      }
+      var info = line.substring(2);
+      switch (line[0]) {
+      case 'l':
+        // leaf name
+        extraInfo.leafName = info;
+        break;
+      case 'm':
+        // marker
+        if (!("marker" in extraInfo)) {
+          extraInfo.marker = [];
+        }
+        extraInfo.marker.push(info);
+        break;
+      case 's':
+        // sample
+        var sampleName = info;
+        var sample = new Sample(sampleName, extraInfo);
+        samples.push(sample);
+        extraInfo = {}; // reset the extra info for future rounds
+        break;
+      }
+    }
+    return samples;
+  },
+
+  parseCallStack: function Parser_parseCallStack(serialization) {
+    return serialization.split(",");
+  },
+
+  convertToCallTree: function Parser_convertToCallTree(samples) {
+    var treeRoot = null;
+    for (var i = 0; i < samples.length; ++i) {
+      var sample = samples[i];
+      var callstack = this.parseCallStack(sample.name);
+      if (!treeRoot) {
+        treeRoot = new TreeNode(callstack[0], null);
+      } else {
+        var newChild = treeRoot.followPath(callstack.slice(1));
+        if (newChild.name == callstack[callstack.length - 1]) {
+          // we found the exact node, so let's just increment
+          // the counters through the parent chain.
+          var node = newChild;
+          while (node) {
+            node.counter++;
+            node = node.parent;
+          }
+        } else {
+          var depth = newChild.depth + 1;
+          var remainingCallstack = callstack.slice(depth);
+          var node = newChild;
+          while (node) {
+            node.counter++;
+            node = node.parent;
+          }
+          node = newChild;
+          for (var j = 0; j < remainingCallstack.length; ++j) {
+            var frame = remainingCallstack[j];
+            var child = new TreeNode(frame, node);
+            node.children.push(child);
+            node = child;
+          }
+        }
+      }
+    }
+    return treeRoot;
+  }
+};
---
+++ ui.js
@@ -0,0 +1,223 @@
+jQuery.jstree.THEMES_DIR = "jstree/themes/";
+
+function TreeRenderer() {}
+TreeRenderer.prototype = {
+  render: function TreeRenderer_render(tree, container) {
+    function convertToJSTreeData(tree) {
+      var object = {};
+      function childVisitor(node, curObj) {
+        curObj.title = node.counter + " " + node.name;
+        if (node.children.length) {
+          curObj.children = [];
+          for (var i = 0; i < node.children.length; ++i) {
+            var child = node.children[i];
+            var newObj = {};
+            childVisitor(child, newObj);
+            curObj.children.push(newObj);
+          }
+        }
+      }
+      childVisitor(tree, object);
+      return {data: [object]};
+    }
+    jQuery(container).jstree({
+      json: convertToJSTreeData(tree),
+      plugins: ["themes", "json", "ui"]
+    });
+  }
+};
+
+function HistogramRenderer() {}
+HistogramRenderer.prototype = {
+  render: function HistogramRenderer_render(data, container) {
+    function convertToHistogramData(data) {
+      var histogramData = [];
+      var prevName = "";
+      var parser = new Parser();
+      for (var i = 0; i < data.length; ++i) {
+        var step = data[i];
+        var name = step.name;
+        var value = parser.parseCallStack(name).length;
+        if ("marker" in step.extraInfo) {
+          // a new marker boundary has been discovered
+          var item = {
+            name: name,
+            width: 1,
+            value: value,
+            marker: step.extraInfo.marker
+          };
+          histogramData.push(item);
+          prevName = name;
+        } else if (name != prevName) {
+          // a new name boundary has been discovered
+          var item = {
+            name: name,
+            width: 1,
+            value: value
+          };
+          histogramData.push(item);
+          prevName = name;
+        } else {
+          // the continuation of the previous data
+          histogramData[histogramData.length - 1].width++;
+        }
+      }
+      return histogramData;
+    }
+    var histogramData = convertToHistogramData(data);
+    var count = histogramData.length;
+    var width = container.clientWidth,
+        height = container.clientHeight;
+
+    // construct the SVG root element
+    const kSVGNS = "http://www.w3.org/2000/svg";
+    var svgRoot = document.createElementNS(kSVGNS, "svg");
+    svgRoot.setAttribute("version", "1.1");
+    svgRoot.setAttribute("baseProfile", "full");
+    svgRoot.setAttribute("width", width);
+    svgRoot.setAttribute("height", height);
+    container.appendChild(svgRoot);
+
+    function createRect(container, x, y, w, h, color) {
+      var rect = document.createElementNS(kSVGNS, "rect");
+      rect.setAttribute("x", x);
+      rect.setAttribute("y", y);
+      rect.setAttribute("width", w);
+      rect.setAttribute("height", h);
+      rect.setAttribute("fill", color);
+      container.appendChild(rect);
+      rect.addEventListener("mouseover", function() {
+        rect.setAttribute("fill-opacity", "0.8");
+      }, false);
+      rect.addEventListener("mouseout", function() {
+        rect.removeAttribute("fill-opacity");
+      }, false);
+      return rect;
+    }
+
+    // iterate over the histogram items and create rects for each one
+    var widthSum = 0, maxHeight = 0;
+    for (var i = 0; i < count; ++i) {
+      var step = histogramData[i];
+      widthSum += step.width;
+      if (step.value > maxHeight) {
+        maxHeight = step.value;
+      }
+    }
+    var widthFactor = width / widthSum;
+    var heightFactor = height / maxHeight;
+    var widthSeenSoFar = 0;
+    for (var i = 0; i < count; ++i) {
+      var step = histogramData[i];
+      var rect = createRect(svgRoot, widthSeenSoFar, 0,
+                            step.width * widthFactor,
+                            step.value * heightFactor,
+                            "blue");
+      if ("marker" in step) {
+        rect.setAttribute("data-marker", step.marker);
+      }
+      widthSeenSoFar += step.width * widthFactor;
+    }
+
+    function gatherMarkersList(histogramData) {
+      var markers = [];
+      for (var i = 0; i < histogramData.length; ++i) {
+        var step = histogramData[i];
+        if ("marker" in step) {
+          markers.push({
+            index: i,
+            name: step.marker
+          });
+        }
+      }
+      return markers;
+    }
+
+    var markers = gatherMarkersList(histogramData);
+    var rangeSelector = new RangeSelector();
+    rangeSelector.render(svgRoot, markers);
+  }
+};
+
+function RangeSelector() {}
+RangeSelector.prototype = {
+  render: function RangeSelector_render(graph, markers) {
+    var select = document.createElement("select");
+    select.setAttribute("multiple", "multiple");
+    select.setAttribute("size", markers.length);
+    graph.parentNode.appendChild(select);
+
+    for (var i = 0; i < markers.length; ++i) {
+      var marker = markers[i];
+      var option = document.createElement("option");
+      option.appendChild(document.createTextNode(marker.name));
+      option.setAttribute("data-index", marker.index);
+      select.appendChild(option);
+    }
+
+    select.addEventListener("change", function(e) {
+      // look for non-consecutive ranges, and make them consecutive
+      var range = [];
+      var children = select.childNodes;
+      for (var i = 0; i < children.length; ++i) {
+        range.push(children[i].selected);
+      }
+      var begin = -1, end = -1;
+      for (var i = 0; i < range.length; ++i) {
+        if (begin == -1 && range[i]) {
+          begin = i;
+        } else if (begin != -1 && range[i]) {
+          end = i;
+        }
+      }
+      for (var i = begin; i <= end; ++i) {
+        children[i].selected = true;
+      }
+      if (end > -1) {
+        for (var i = end + 1; i < children.length; ++i) {
+          children[i].selected = false;
+        }
+      }
+
+      // highlight the range in the histogram
+      const hiliteClassName = "histogramHilite";
+      var prevHilite = document.querySelector("." + hiliteClassName);
+      if (prevHilite) {
+        prevHilite.parentNode.removeChild(prevHilite);
+      }
+      function rect(index) {
+        return graph.childNodes[children[index].getAttribute("data-index")];
+      }
+      if (end > begin) {
+        var hilite = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        hilite.setAttribute("x", rect(begin).getAttribute("x"));
+        hilite.setAttribute("y", 0);
+        hilite.setAttribute("width", parseFloat(rect(end).getAttribute("width")) +
+                                     parseFloat(rect(end).getAttribute("x")) -
+                                     parseFloat(rect(begin).getAttribute("x")));
+        hilite.setAttribute("height", graph.getAttribute("height"));
+        hilite.setAttribute("fill", "gray");
+        hilite.setAttribute("fill-opacity", "0.5");
+        hilite.setAttribute("class", hiliteClassName);
+        hilite.setAttribute("style", "pointer-events: none");
+        graph.appendChild(hilite);
+      }
+    }, false);
+  }
+};
+
+function parse() {
+  var parser = new Parser();
+  var data = parser.parse(document.getElementById("data").value);
+  var treeData = parser.convertToCallTree(data);
+  var tree = document.createElement("div");
+  document.body.appendChild(tree);
+  var treeRenderer = new TreeRenderer();
+  treeRenderer.render(treeData, tree);
+  var histogram = document.createElement("div");
+  histogram.style.width = "800px";
+  histogram.style.height = "400px";
+  document.body.appendChild(histogram);
+  var histogramRenderer = new HistogramRenderer();
+  histogramRenderer.render(data, histogram);
+}
