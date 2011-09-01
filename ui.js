d8586440610edde326f27f15bfa0c137e4f6fd0f
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-09-01 18:53:42 -0400

    Import the latest stuff on master

--- ui.js
+++ ui.js
@@ -1,5 +1,13 @@
 jQuery.jstree.THEMES_DIR = "jstree/themes/";
 
+const hiliteClassName = "histogramHilite";
+
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
 function TreeRenderer() {}
 TreeRenderer.prototype = {
   render: function TreeRenderer_render(tree, container) {
@@ -22,6 +30,8 @@ TreeRenderer.prototype = {
       childVisitor(tree, object);
       return {data: [object]};
     }
+    removeAllChildren(container);
+    container.className = "";
     jQuery(container).jstree({
       json: convertToJSTreeData(tree),
       plugins: ["themes", "json", "ui"]
@@ -71,6 +81,26 @@ HistogramRenderer.prototype = {
     var count = histogramData.length;
     var width = container.clientWidth,
         height = container.clientHeight;
+
+    removeAllChildren(container);
+    removeAllChildren(markerContainer);
+
+    // Add the filtering UI
+    var iconBox = document.createElement("div");
+    iconBox.setAttribute("id", "iconbox");
+    var filterButton = document.createElement("img");
+    filterButton.setAttribute("src", "images/filter.png");
+    filterButton.setAttribute("id", "filter");
+    filterButton.setAttribute("class", "hidden");
+    filterButton.setAttribute("title", "Show only the samples from the selected range");
+    iconBox.appendChild(filterButton);
+    var showallButton = document.createElement("img");
+    showallButton.setAttribute("src", "images/showall.png");
+    showallButton.setAttribute("id", "showall");
+    showallButton.setAttribute("class", "hidden");
+    showallButton.setAttribute("title", "Show all of the samples");
+    iconBox.appendChild(showallButton);
+    container.appendChild(iconBox);
 
     // construct the SVG root element
     const kSVGNS = "http://www.w3.org/2000/svg";
@@ -160,6 +190,7 @@ HistogramRenderer.prototype = {
     var markers = gatherMarkersList(histogramData);
     var rangeSelector = new RangeSelector(markerContainer);
     rangeSelector.render(svgRoot, markers);
+    rangeSelector.enableRangeSelectionOnHistogram(svgRoot);
   }
 };
 
@@ -168,10 +199,13 @@ function RangeSelector(container) {
 }
 RangeSelector.prototype = {
   render: function RangeSelector_render(graph, markers) {
+    removeAllChildren(markers);
+
     var select = document.createElement("select");
     select.setAttribute("multiple", "multiple");
     select.setAttribute("size", markers.length);
     this.container.appendChild(select);
+    this.selector = select;
 
     for (var i = 0; i < markers.length; ++i) {
       var marker = markers[i];
@@ -181,7 +215,26 @@ RangeSelector.prototype = {
       select.appendChild(option);
     }
 
-    select.addEventListener("change", function(e) {
+    // Prepare the filtering UI
+    var self = this;
+    var filter = document.getElementById("filter");
+    try {
+      filter.removeEventListener("click", filter_onClick, false);
+    } catch (err) {
+    }
+    filter.addEventListener("click", function filter_onClick() {
+      self.filterCurrentRange(graph);
+    }, false);
+
+    try {
+      select.removeEventListener("click", select_onChange, false);
+    } catch (err) {
+    }
+    select.addEventListener("change", function select_onChange(e) {
+      if (self.changeEventSuppressed) {
+        return;
+      }
+
       // look for non-consecutive ranges, and make them consecutive
       var range = [];
       var children = select.childNodes;
@@ -196,8 +249,10 @@ RangeSelector.prototype = {
           end = i;
         }
       }
-      for (var i = begin; i <= end; ++i) {
-        children[i].selected = true;
+      if (begin > -1) {
+        for (var i = begin; i <= end; ++i) {
+          children[i].selected = true;
+        }
       }
       if (end > -1) {
         for (var i = end + 1; i < children.length; ++i) {
@@ -206,11 +261,11 @@ RangeSelector.prototype = {
       }
 
       // highlight the range in the histogram
-      const hiliteClassName = "histogramHilite";
       var prevHilite = document.querySelector("." + hiliteClassName);
       if (prevHilite) {
         prevHilite.parentNode.removeChild(prevHilite);
       }
+      document.getElementById("filter").className = "hidden";
       const hilitedMarker = "markerHilite";
       var prevMarkerHilite = document.querySelector("#" + hilitedMarker);
       if (prevMarkerHilite) {
@@ -225,29 +280,139 @@ RangeSelector.prototype = {
         rect(begin).setAttribute("id", hilitedMarker);
         rect(begin).setAttribute("style", "fill: red;");
       } else if (end > begin) {
-        var hilite = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        hilite.setAttribute("x", rect(begin).getAttribute("x"));
-        hilite.setAttribute("y", 0);
-        hilite.setAttribute("width", parseFloat(rect(end).getAttribute("width")) +
-                                     parseFloat(rect(end).getAttribute("x")) -
-                                     parseFloat(rect(begin).getAttribute("x")));
-        hilite.setAttribute("height", graph.getAttribute("height"));
-        hilite.setAttribute("fill", "gray");
-        hilite.setAttribute("fill-opacity", "0.5");
-        hilite.setAttribute("class", hiliteClassName);
-        hilite.setAttribute("style", "pointer-events: none");
-        graph.appendChild(hilite);
+        self.drawHiliteRectangle(graph,
+                                 rect(begin).getAttribute("x"),
+                                 0,
+                                 parseFloat(rect(end).getAttribute("width")) +
+                                 parseFloat(rect(end).getAttribute("x")) -
+                                 parseFloat(rect(begin).getAttribute("x")),
+                                 graph.getAttribute("height"));
       }
     }, false);
+  },
+  drawHiliteRectangle: function RangeSelector_drawHiliteRectangle(graph, x, y, width, height) {
+    var hilite = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    hilite.setAttribute("x", x);
+    hilite.setAttribute("y", 0);
+    hilite.setAttribute("width", width);
+    hilite.setAttribute("height", height);
+    hilite.setAttribute("fill", "gray");
+    hilite.setAttribute("fill-opacity", "0.5");
+    hilite.setAttribute("class", hiliteClassName);
+    hilite.setAttribute("style", "pointer-events: none");
+    graph.appendChild(hilite);
+    document.getElementById("filter").className = "";
+    return hilite;
+  },
+  clearCurrentRangeSelection: function RangeSelector_clearCurrentRangeSelection() {
+    try {
+      this.changeEventSuppressed = true;
+      var children = this.selector.childNodes;
+      for (var i = 0; i < children.length; ++i) {
+        children[i].selected = false;
+      }
+    } finally {
+      this.changeEventSuppressed = false;
+    }
+  },
+  enableRangeSelectionOnHistogram: function RangeSelector_enableRangeSelectionOnHistogram(graph) {
+    var isDrawingRectangle = false;
+    var origX, origY;
+    var hilite = null;
+    var self = this;
+    function updateHiliteRectangle(newX, newY) {
+      var startX = Math.min(newX, origX) - graph.parentNode.offsetLeft;
+      var startY = 0;
+      var width = Math.abs(newX - origX);
+      var height = graph.parentNode.clientHeight;
+      if (hilite) {
+        hilite.setAttribute("x", startX);
+        hilite.setAttribute("y", startY);
+        hilite.setAttribute("width", width);
+        hilite.setAttribute("height", height);
+      } else {
+        if (width) {
+          var prevHilite = document.querySelector("." + hiliteClassName);
+          if (prevHilite) {
+            prevHilite.parentNode.removeChild(prevHilite);
+          }
+          self.clearCurrentRangeSelection();
+          hilite = self.drawHiliteRectangle(graph, startX, startY, width, height);
+        }
+      }
+    }
+    graph.addEventListener("mousedown", function(e) {
+      isDrawingRectangle = true;
+      origX = e.pageX;
+      origY = e.pageY;
+    }, false);
+    graph.addEventListener("mouseup", function(e) {
+      updateHiliteRectangle(e.pageX, e.pageY);
+      isDrawingRectangle = false;
+      hilite = null;
+    }, false);
+    graph.addEventListener("mousemove", function(e) {
+      if (isDrawingRectangle) {
+        updateHiliteRectangle(e.pageX, e.pageY);
+      }
+    }, false);
+  },
+  filterCurrentRange: function RangeSelector_filterCurrentRange(graph) {
+    // First, retrieve the current range of filtered samples
+    function sampleIndexFromPoint(x) {
+      var totalSamples = parseFloat(gVisibleRange.end - gVisibleRange.start - 1);
+      var width = parseFloat(graph.parentNode.clientWidth);
+      var factor = totalSamples / width;
+      return parseInt(parseFloat(x) * factor);
+    }
+
+    var hiliteRect = document.querySelector("." + hiliteClassName);
+    var start = sampleIndexFromPoint(hiliteRect.getAttribute("x"));
+    var end = sampleIndexFromPoint(parseFloat(hiliteRect.getAttribute("x")) +
+                                   parseFloat(hiliteRect.getAttribute("width")));
+    displaySample(start, end + 1);
+
+    var self = this;
+    var showall = document.getElementById("showall");
+    try {
+      showall.removeEventListener("click", showall_onClick, false);
+    } catch (err) {
+    }
+    showall.addEventListener("click", function showall_onClick() {
+      self.resetFilter();
+    }, false);
+    showall.className = "";
+  },
+  resetFilter: function RangeSelector_resetFilter() {
+    displaySample(0, gSamples.length);
+    document.getElementById("showall").className = "hidden";
+  }
+};
+
+var gSamples = [];
+var gVisibleRange = {
+  start: -1,
+  end: -1,
+  filter: function(start, end) {
+    this.start = start;
+    this.end = end;
+    return gSamples.slice(start, end);
   }
 };
 
 function parse() {
+  var parser = new Parser();
+  gSamples = parser.parse(document.getElementById("data").value);
+  displaySample(0, gSamples.length);
+}
+
+function displaySample(start, end) {
   document.getElementById("dataentry").className = "hidden";
   document.getElementById("ui").className = "";
 
+  var data = gVisibleRange.filter(start, end);
+
   var parser = new Parser();
-  var data = parser.parse(document.getElementById("data").value);
   var treeData = parser.convertToCallTree(data);
   var tree = document.getElementById("tree");
   var treeRenderer = new TreeRenderer();
