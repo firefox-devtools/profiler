d3e441cc10fedfff7015268b8cf19e7211056c86
Benoit Girard <bgirard@mozilla.com>
2011-12-24 14:57:29 -0500

    Added selection

--- tree.js
+++ tree.js
@@ -1,6 +1,7 @@
 function Tree(root, data) {
   this.root = root;
   this.init(data);
+  this.selected = null;
 };
 
 Tree.prototype = {
@@ -21,12 +22,21 @@ Tree.prototype = {
     div.className = "collapsed";
     div.onclick = this._onclick;
     div.onkeypress = this._onkeypress;
-    div.appendChild(document.createTextNode(data.title));
+    var text = document.createElement("a");
+    text.innerHTML = data.title;
+    text.node = div;
+    div.node = div;
+    div.treeText = text;
+    div.treeText.className = "unselected";
+    div.appendChild(text);
+    div.treeChildren = [];
+    div.tree = this;
     if ("children" in data) {
       for (var i = 0; i < data.children.length; ++i) {
         var innerTree = this._createTree(div, data.children[i]);
         if (innerTree) {
           div.appendChild(innerTree);
+          div.treeChildren.push(innerTree);
         }
       }
     }
@@ -35,8 +45,19 @@ Tree.prototype = {
   _toggle: function Tree__toggle(div) {
     div.className = (div.className == "collapsed") ? "" : "collapsed";
   },
+  _select: function Tree__select(div) {
+    if (div.tree.selected != null) {
+      div.tree.selected.treeText.className = "unselected";
+      div.tree.selected = null;
+    }
+    if (div != null) {
+      div.treeText.className = "selected";
+      div.tree.selected = div;
+    }
+  },
   _onclick: function Tree__onclick(event) {
-    Tree.prototype._toggle(event.target);
+    Tree.prototype._toggle(event.target.node);
+    Tree.prototype._select(event.target.node);
     event.stopPropagation();
   },
   _onkeypress: function Tree__onkeypress(event) {
