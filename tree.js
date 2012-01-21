4b7eb2e414f28985aa39c549c5af63da7fea9ceb
Benoit Girard <b56girard@gmail.com>
2012-01-21 10:45:20 -0500

    Progress on keyevents

--- tree.js
+++ tree.js
@@ -21,7 +21,6 @@ Tree.prototype = {
     var div = document.createElement("div");
     div.className = "collapsed";
     div.onclick = this._onclick;
-    div.onkeypress = this._onkeypress;
     var text = document.createElement("a");
     text.innerHTML = data.title;
     text.node = div;
@@ -40,20 +39,30 @@ Tree.prototype = {
         }
       }
     }
+    document.onkeypress = this._onkeypress;
     return div;
   },
   _toggle: function Tree__toggle(div) {
     div.className = (div.className == "collapsed") ? "" : "collapsed";
   },
   _select: function Tree__select(div) {
+    document.onkeypress = this._onkeypress;
     if (div.tree.selected != null) {
+      div.tree.selected.className = "";
       div.tree.selected.treeText.className = "unselected";
       div.tree.selected = null;
     }
     if (div != null) {
+      div.className = "selected_treenode";
       div.treeText.className = "selected";
       div.tree.selected = div;
     }
+  },
+  _selected: function Tree__selected() {
+    return document.getElementsByClassName("selected_treenode")[0];
+  },
+  _isCollapsed: function Tree__isCollapsed(div) {
+    return (div.className == "collapsed");
   },
   _onclick: function Tree__onclick(event) {
     Tree.prototype._toggle(event.target.node);
@@ -61,7 +70,17 @@ Tree.prototype = {
     event.stopPropagation();
   },
   _onkeypress: function Tree__onkeypress(event) {
-    // handle keypresses
+    var selected = Tree.prototype._selected();
+    if (selected == null) return;
+    if (event.keyCode == 37) {
+      var isCollapsed = Tree.prototype._isCollapsed(selected);
+      if (!isCollapsed) {
+        Tree.prototype._toggle(selected);
+      } else {
+        // select parent
+      }
+    }
+    event.stopPropagation();
   },
 };
 
