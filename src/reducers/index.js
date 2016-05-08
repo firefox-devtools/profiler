dddd2c68cf201aec3560319ac4f91093dc3fa531
Markus Stange <mstange@themasta.com>
2016-05-08 17:33:58 -0400

    Use combineReducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,57 @@
+import { combineReducers } from 'redux';
 import { applyFunctionMerging, setFuncNames } from '../symbolication';
 import { defaultThreadOrder } from '../profile-data';
 
-function profileViewReducer(state, action) {
+function status(state = 'INITIALIZING', action) {
   switch (action.type) {
-    case 'START_SYMBOLICATING':
-      return Object.assign({}, state, { symbolicationStatus: 'SYMBOLICATING' });
-    case 'DONE_SYMBOLICATING':
-      let newState = Object.assign({}, state, { symbolicationStatus: 'DONE' });
-      delete newState.waitingForLibs;
-      return newState;
-    case 'REQUESTING_SYMBOL_TABLE': {
-      let newWaitingForLibs = new Set(state.waitingForLibs || []);
-      newWaitingForLibs.add(action.requestedLib);
-      return Object.assign({}, state, { waitingForLibs: newWaitingForLibs });
-    }
-    case 'RECEIVED_SYMBOL_TABLE_REPLY': {
-      let newWaitingForLibs = new Set(state.waitingForLibs || []);
-      newWaitingForLibs.delete(action.requestedLib);
-      return Object.assign({}, state, { waitingForLibs: newWaitingForLibs });
-    }
+    case 'WAITING_FOR_PROFILE_FROM_ADDON':
+      return 'WAITING_FOR_PROFILE';
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return 'DONE';
+    default:
+      return state;
+  }
+}
+
+function view(state = 'INITIALIZING', action) {
+  switch (action.type) {
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return 'PROFILE';
+    default:
+      return state;
+  }
+}
+
+function threadOrder(state = [], action) {
+  switch (action.type) {
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return defaultThreadOrder(action.profile.threads);
+    default:
+      return state;
+  }
+}
+
+function selectedThread(state = 0, action) {
+  switch (action.type) {
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return defaultThreadOrder(action.profile.threads)[0];
+    case 'CHANGE_SELECTED_THREAD':
+      return action.selectedThread;
+    default:
+      return state;
+  }
+}
+
+function viewOptionsThreads(state = [], action) {
+  switch (action.type) {
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return action.profile.threads.map(thread => ({
+        selectedFuncStack: [],
+        expandedFuncStacks: [],
+      }));
     case 'COALESCED_FUNCTIONS_UPDATE': {
       const { functionsUpdatePerThread } = action;
-      const threads = state.profile.threads.map((thread, threadIndex) => {
-        if (!functionsUpdatePerThread[threadIndex])
-          return thread;
-        const { oldFuncToNewFuncMap, funcIndices, funcNames } = functionsUpdatePerThread[threadIndex];
-        thread = applyFunctionMerging(thread, oldFuncToNewFuncMap);
-        thread = setFuncNames(thread, funcIndices, funcNames);
-        return thread;
-      });
-      const profile = Object.assign({}, state.profile, { threads });
-      const vothreads = state.viewOptions.threads.map((thread, threadIndex) => {
+      return state.map((thread, threadIndex) => {
         if (!functionsUpdatePerThread[threadIndex]) {
           return thread;
         }
@@ -45,73 +66,82 @@ function profileViewReducer(state, action) {
             return newFunc === undefined ? oldFunc : newFunc;
           });
         });
-        return Object.assign({}, thread, { selectedFuncStack, expandedFuncStacks });
+        return { selectedFuncStack, expandedFuncStacks };
       });
-      const viewOptions = Object.assign({}, state.viewOptions, { threads: vothreads });
-      return Object.assign({}, state, { profile, viewOptions });
     }
     case 'CHANGE_SELECTED_FUNC_STACK': {
       const { selectedFuncStack, threadIndex } = action;
-      const threads = state.viewOptions.threads.map((thread, ti) => {
+      return state.map((thread, ti) => {
         if (ti !== threadIndex) {
           return thread;
         }
         return Object.assign({}, thread, { selectedFuncStack });
       });
-      const viewOptions = Object.assign({}, state.viewOptions, { threads });
-      return Object.assign({}, state, { viewOptions });
-    }
-    case 'CHANGE_SELECTED_THREAD': {
-      const { selectedThread } = action;
-      const viewOptions = Object.assign({}, state.viewOptions, { selectedThread });
-      return Object.assign({}, state, { viewOptions });
     }
     case 'CHANGE_EXPANDED_FUNC_STACKS': {
       const { threadIndex, expandedFuncStacks } = action;
-      const threads = state.viewOptions.threads.map((thread, ti) => {
+      return state.map((thread, ti) => {
         if (ti !== threadIndex) {
           return thread;
         }
         return Object.assign({}, thread, { expandedFuncStacks });
       });
-      const viewOptions = Object.assign({}, state.viewOptions, { threads });
-      return Object.assign({}, state, { viewOptions });
     }
     default:
       return state;
   }
 }
 
-export default function reducer(state = {}, action) {
+function symbolicationStatus(state = 'DONE', action) {
   switch (action.type) {
-    case 'WAITING_FOR_PROFILE_FROM_ADDON':
-      return { status: 'WAITING_FOR_PROFILE' };
-    case 'RECEIVE_PROFILE_FROM_ADDON':
-      const threadOrder = defaultThreadOrder(action.profile.threads);
-      return {
-        status: 'DONE',
-        view: 'PROFILE',
-        profileView: {
-          viewOptions: {
-            threadOrder: threadOrder,
-            selectedThread: threadOrder[0],
-            threads: action.profile.threads.map(thread => ({
-              selectedFuncStack: [],
-              expandedFuncStacks: [],
-            })),
-            symbolicationStatus: null,
-          },
-          profile: action.profile
-        },
-      };
+    case 'START_SYMBOLICATING':
+      return 'SYMBOLICATING';
+    case 'DONE_SYMBOLICATING':
+      return 'DONE';
     default:
-      if ('profileView' in state) {
-        const profileView = profileViewReducer(state.profileView, action);
-        if (profileView !== state.profileView) {
-          return Object.assign({}, state, { profileView });
-        }
-        return state;
-      }
       return state;
   }
 }
+
+function waitingForLibs(state = new Set(), action) {
+  switch (action.type) {
+    case 'REQUESTING_SYMBOL_TABLE': {
+      const newState = new Set(state);
+      newState.add(action.requestedLib);
+      return newState;
+    }
+    case 'RECEIVED_SYMBOL_TABLE_REPLY': {
+      const newState = new Set(state);
+      newState.delete(action.requestedLib);
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
+
+function profile(state = {}, action) {
+  switch (action.type) {
+    case 'RECEIVE_PROFILE_FROM_ADDON':
+      return action.profile;
+    case 'COALESCED_FUNCTIONS_UPDATE': {
+      const { functionsUpdatePerThread } = action;
+      const threads = state.threads.map((thread, threadIndex) => {
+        if (!functionsUpdatePerThread[threadIndex])
+          return thread;
+        const { oldFuncToNewFuncMap, funcIndices, funcNames } = functionsUpdatePerThread[threadIndex];
+        thread = applyFunctionMerging(thread, oldFuncToNewFuncMap);
+        thread = setFuncNames(thread, funcIndices, funcNames);
+        return thread;
+      });
+      return Object.assign({}, state, { threads });
+    }
+    default:
+      return state;
+  }
+}
+
+const viewOptions = combineReducers({ threadOrder, selectedThread, threads: viewOptionsThreads, symbolicationStatus, waitingForLibs });
+const profileView = combineReducers({ viewOptions, profile });
+
+export default combineReducers({ status, view, profileView });
