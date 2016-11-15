3bed9e2f1f15f657040d106c5813d0857030308a
Greg Tatum <tatum.creative@gmail.com>
2016-11-15 14:59:04 -0600

    Add npm run start-no-hot command to opt out of hot reloading

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
+  hot: process.env.NODE_ENV === 'development' ? true : false,
   historyApiFallback: true,
   stats: {
-    colors: true
-  }
+    colors: true,
+  },
 }).listen(4242, 'localhost', function (err) {
   if (err) {
     console.log(err);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: [
-    './index'
+    './index',
   ],
   output: {
     path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
-    })
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
+    }),
   ],
   resolve: {
     alias: {
       'redux-devtools/lib': path.join(__dirname, '..', '..', 'src'),
       'redux-devtools': path.join(__dirname, '..', '..', 'src'),
-      'react': path.join(__dirname, 'node_modules', 'react')
+      'react': path.join(__dirname, 'node_modules', 'react'),
     },
-    extensions: ['', '.js']
+    extensions: ['', '.js'],
   },
   resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
+    'fallback': path.join(__dirname, 'node_modules'),
   },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       exclude: /node_modules/,
-      include: __dirname
+      include: __dirname,
     }, {
       test: /\.css?$/,
       loaders: ['style', 'raw'],
-      include: __dirname
-    }]
-  }
-}
+      include: __dirname,
+    }],
+  },
+};
 
 if (process.env.NODE_ENV === 'development') {
   module.exports.devtool = 'cheap-module-eval-source-map';
@@ -47,4 +47,7 @@ if (process.env.NODE_ENV === 'development') {
     'webpack/hot/only-dev-server',
     'react-hot-loader/patch'].concat(module.exports.entry);
   module.exports.plugins = [new webpack.HotModuleReplacementPlugin()].concat(module.exports.plugins);
+} else if (process.env.NODE_ENV === 'development-no-hot') {
+  module.exports.devtool = 'source-map';
+  module.exports.entry = ['webpack-dev-server/client?http://localhost:4242'].concat(module.exports.entry);
 }
