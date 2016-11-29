b0d9f422e658f7c2833f115380c788dcf789c5a0
Markus Stange <mstange@themasta.com>
2016-11-29 00:47:57 -0500

    Fix eslint issue.

--- src/shorten-cleopatra-url.js
+++ src/shorten-cleopatra-url.js
@@ -5,8 +5,8 @@ export function shortenCleopatraUrl(longUrl) {
   const bitlyQueryUrl = 'https://api-ssl.bitly.com/v3/shorten?' +
     queryString.stringify({
       longUrl,
-      domain: 'clptr.io',
-      access_token: 'd51e4336df90de42b466d731f7534c2e5eaad3bc',
+      'domain': 'clptr.io',
+      'access_token': 'd51e4336df90de42b466d731f7534c2e5eaad3bc',
     });
   return fetchJsonP(bitlyQueryUrl).then(response => response.json()).then(json => json.data.url);
 }
