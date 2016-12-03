19737b07c25722f3ea18dc3884d1e1b56d6c47c3
Markus Stange <mstange@themasta.com>
2016-12-02 19:27:46 -0500

    Make thread stack graph more opaque when zoomed far out.

--- src/content/components/ThreadStackGraph.js
+++ src/content/components/ThreadStackGraph.js
@@ -63,7 +63,7 @@ class ThreadStackGraph extends Component {
     const yPixelsPerDepth = c.height / maxDepth;
     const trueIntervalPixelWidth = interval * xPixelsPerMs;
     const multiplier = trueIntervalPixelWidth < 2.0 ? 1.2 : 1.0;
-    const drawnIntervalTime = interval * multiplier;
+    const drawnIntervalWidth = Math.max(0.8, trueIntervalPixelWidth * multiplier);
     let selectedFuncStackDepth = 0;
     if (selectedFuncStack !== -1 && selectedFuncStack !== null) {
       selectedFuncStackDepth = funcStackTable.depth[selectedFuncStack];
@@ -78,7 +78,7 @@ class ThreadStackGraph extends Component {
     }
     for (let i = 0; i < sampleFuncStacks.length; i++) {
       const sampleTime = thread.samples.time[i];
-      if (sampleTime + drawnIntervalTime < range[0] || sampleTime > range[1]) {
+      if (sampleTime + drawnIntervalWidth / xPixelsPerMs < range[0] || sampleTime > range[1]) {
         continue;
       }
       const funcStack = sampleFuncStacks[i];
@@ -88,7 +88,7 @@ class ThreadStackGraph extends Component {
       // const responsiveness = thread.samples.responsiveness[i];
       // const jankSeverity = Math.min(1, responsiveness / 100);
       ctx.fillStyle = isHighlighted ? '#38445f' : '#7990c8';
-      ctx.fillRect((sampleTime - range[0]) * xPixelsPerMs, startY, drawnIntervalTime * xPixelsPerMs, sampleHeight);
+      ctx.fillRect((sampleTime - range[0]) * xPixelsPerMs, startY, drawnIntervalWidth, sampleHeight);
     }
 
   }
