9c42094bfbff5a913fccd9410e7066d840cf5bde
Markus Stange <mstange@themasta.com>
2016-12-04 15:18:58 -1000

    Extract the generic parts of ProfileFilterNavigator into a FilterNavigatorBar component.

---
+++ src/content/components/FilterNavigatorBar.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+
+import './FilterNavigatorBar.css';
+
+class FilterNavigatorBar extends Component {
+  constructor(props) {
+    super(props);
+    this._onLiClick = this._onLiClick.bind(this);
+  }
+
+  _onLiClick(e) {
+    const index = e.target.getAttribute('data-index')|0;
+    this.props.onPop(index);
+  }
+
+  render() {
+    const { className, items, selectedItem } = this.props;
+    return (
+      <ol className={classNames('filterNavigatorBar', className)}>
+        {
+          items.map((item, i) => (
+            <li key={i}
+                data-index={i}
+                className={classNames(
+                  'filterNavigatorBarItem', {
+                    'filterNavigatorBarRootItem': i === 0,
+                    'filterNavigatorBarBeforeSelectedItem': i === selectedItem - 1,
+                    'filterNavigatorBarSelectedItem': i === selectedItem,
+                    'filterNavigatorBarLeafItem': i === items.length - 1,
+                  })}
+                onClick={this._onLiClick}>
+              {item}
+            </li>
+          ))
+        }
+      </ol>
+    );
+  }
+}
+
+FilterNavigatorBar.propTypes = {
+  className: PropTypes.string,
+  items: PropTypes.array.isRequired,
+  selectedItem: PropTypes.number.isRequired,
+  onPop: PropTypes.func.isRequired,
+};
+
+export default FilterNavigatorBar;
--- src/content/containers/ProfileFilterNavigator.js
+++ src/content/containers/ProfileFilterNavigator.js
@@ -1,70 +1,22 @@
-import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { getRangeFilters } from '../selectors';
+import FilterNavigatorBar from '../components/FilterNavigatorBar';
 
-// const MyLink = ({ to, children, push }) => {
-//   // Ideally this should be an <a href>, but need a way to convert 'to' (which
-//   // is a location-like object) to a string href.
-//   return <span onClick={e => {push(to); e.preventDefault(); }}>{children}</span>;
-// };
-
-class ProfileFilterNavigator extends Component {
-  render() {
-    const { rangeFilters, popRangeFiltersAndUnsetSelection, location } = this.props;
-    const rangeItems = rangeFilters.map(range => `Range: ${(range.start / 1000).toFixed(2)}s–${(range.end / 1000).toFixed(2)}s`);
-    const items = ['Complete Profile', ...rangeItems];
-    const selectedItem = items.length - 1;
-    return (
-      <ol className='profileFilterNavigator'>
-        {
-          items.map((item, i) => {
-            const classList = ['profileFilterNavigatorItem'];
-            if (i === 0) {
-              classList.push('profileFilterNavigatorRootItem');
-            }
-            if (i === selectedItem - 1) {
-              classList.push('profileFilterNavigatorBeforeSelectedItem');
-            } else if (i === selectedItem) {
-              classList.push('profileFilterNavigatorSelectedItem');
-            }
-            if (i === items.length - 1) {
-              classList.push('profileFilterNavigatorLeafItem');
-            }
-            // I'd like to have a link here instead of a <li onclick>, but it's tricky.
-            // There must be a better way to do it than this...
-            // const targetLocation = {
-            //   query: actions.queryRootReducer(location.query, {
-            //     type: 'POP_RANGE_FILTERS',
-            //     firstRemovedFilterIndex: i,
-            //   }),
-            // };
-            // return <li key={i} className={classList.join(' ')}><MyLink to={targetLocation} push={push}>{item}</MyLink></li>;
-            return (
-              <li key={i}
-                  className={classList.join(' ')}
-                  onClick={() => popRangeFiltersAndUnsetSelection(i, location)}>
-                {item}
-              </li>
-            );
-          })
-        }
-      </ol>
-    );
-    // <li className='profileFilterNavigatorItem profileFilterNavigatorRootItem'>Complete profile</li>
-    // <li className='profileFilterNavigatorItem profileFilterNavigatorBeforeSelectedItem'>Range: 2.12s-5.23s</li>
-    // <li className='profileFilterNavigatorItem profileFilterNavigatorSelectedItem'>Range: 2.12s-5.23s</li>
-    // <li className='profileFilterNavigatorItem'>Range: 2.12s-5.23s</li>
-    // <li className='profileFilterNavigatorItem profileFilterNavigatorLeafItem'>Range: 2.12s-5.23s</li>
-  }
+function rangeString(range) {
+  return `Range: ${(range.start / 1000).toFixed(2)}s–${(range.end / 1000).toFixed(2)}s`;
 }
 
-ProfileFilterNavigator.propTypes = {
-  rangeFilters: PropTypes.array.isRequired,
-  popRangeFiltersAndUnsetSelection: PropTypes.func.isRequired,
-  location: PropTypes.object.isRequired,
-};
-
-export default connect((state, props) => ({
-  rangeFilters: getRangeFilters(state, props),
-}), actions)(ProfileFilterNavigator);
+export default connect((state, props) => {
+  const items = ['Complete Profile', ...getRangeFilters(state, props).map(rangeString)];
+  return {
+    className: 'profileFilterNavigator',
+    items,
+    selectedItem: items.length - 1,
+  };
+}, actions, (stateProps, dispatchProps, ownProps) => ({
+  className: stateProps.className,
+  items: stateProps.items,
+  selectedItem: stateProps.selectedItem,
+  onPop: i => dispatchProps.popRangeFiltersAndUnsetSelection(i, ownProps.location),
+}))(FilterNavigatorBar);
