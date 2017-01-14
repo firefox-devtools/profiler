3577ca312b5d6d23fcb216ef108d5080fb11f0de
Bryan Clark <clarkbw@gmail.com>
2017-01-14 10:13:22 +0800

    add some simple tests for worker reducers

--- src/worker/reducers/index.js
+++ src/worker/reducers/index.js
@@ -1,22 +1,6 @@
 import {combineReducers} from 'redux';
-
-function profile(state = null, action) {
-  switch (action.type) {
-    case 'PROFILE_PROCESSED':
-      return action.profile;
-    default:
-      return state;
-  }
-}
-
-function summary(state = null, action) {
-  switch (action.type) {
-    case 'PROFILE_SUMMARY_PROCESSED':
-      return action.summary;
-    default:
-      return state;
-  }
-}
+import profile from './profile'
+import summary from './summary'
 
 export default combineReducers({
   profile,
---
+++ src/worker/test/reducers.js
@@ -0,0 +1,61 @@
+import 'babel-polyfill';
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+import profile from '../reducers/profile';
+import summary from '../reducers/summary';
+
+describe('worker/reducers', function () {
+
+  describe('reducers/profile', function () {
+
+    it('returns the profile', function () {
+      const data = "____DATA____";
+      let state = profile(null, {
+        type: 'PROFILE_PROCESSED',
+        profile: data
+      });
+      assert.strictEqual(data, state);
+    });
+
+    it('defaults to returning the state', function () {
+      let state = profile(undefined, {
+        type: null
+      });
+      assert.equal(null, state);
+
+      const data = {};
+      state = profile(data, {
+        type: null
+      });
+      assert.strictEqual(data, state);
+    });
+
+  });
+
+  describe('reducers/summary', function () {
+
+    it('returns the summary', function () {
+      const data = "____DATA____";
+      let state = summary(null, {
+        type: 'PROFILE_SUMMARY_PROCESSED',
+        summary: data
+      });
+      assert.strictEqual(data, state);
+    });
+
+    it('defaults to returning the state', function () {
+      let state = summary(undefined, {
+        type: null
+      });
+      assert.equal(null, state);
+
+      const data = {};
+      state = summary(data, {
+        type: null
+      });
+      assert.strictEqual(data, state);
+    });
+
+  });
+});
