3f77bc679a8e577eaabe065ff93d2c61a4873e69
Greg Tatum <tatum.creative@gmail.com>
2017-01-31 10:38:07 -0600

    Move index.js to src/content/index.js to speed up webpack

--- src/content/index.js
+++ src/content/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Perf from 'react-addons-perf';
 import { render } from 'react-dom';
-import Root from './src/content/containers/Root';
-import createStore from './src/content/create-store';
-import './res/style.css';
+import Root from './containers/Root';
+import createStore from './create-store';
+import '../../res/style.css';
 
 if (process.env.NODE_ENV === 'production') {
   const runtime = require('offline-plugin/runtime');
