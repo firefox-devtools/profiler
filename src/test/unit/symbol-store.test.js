4b636652697903ed73c0b1f5f1f4737a0b9f9e17
Julien Wajsberg <felash@gmail.com>
2017-05-24 14:54:13 +0200

    Fix and add SymbolStore tests (#347)

--- src/test/unit/symbol-store.test.js
+++ src/test/unit/symbol-store.test.js
@@ -10,21 +10,19 @@ import fakeIndexedDB from 'fake-indexeddb';
 import FDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange';
 
 describe('SymbolStore', function () {
-  let requestCount = 0;
-  const symbolProvider = {
-    requestSymbolTable: () => {
-      requestCount++;
-      return Promise.resolve(exampleSymbolTable);
-    },
-  };
+  let symbolProvider, symbolStore;
 
-  let symbolStore;
+  function deleteDatabase() {
+    return new Promise((resolve, reject) => {
+      const req = fakeIndexedDB.deleteDatabase('perf-html-async-storage-symbol-tables');
+      req.onsuccess = () => resolve();
+      req.onerror = () => reject(req.error);
+    });
+  }
 
   beforeAll(function () {
     global.window = { indexedDB: fakeIndexedDB, IDBKeyRange: FDBKeyRange };
     global.TextDecoder = TextDecoder;
-
-    symbolStore = new SymbolStore('perf-html-async-storage', symbolProvider);
   });
 
   afterAll(function () {
@@ -34,27 +32,52 @@ describe('SymbolStore', function () {
     symbolStore = null;
   });
 
+  beforeEach(function () {
+    symbolProvider = {
+      requestSymbolTable: jest.fn(() => Promise.resolve(exampleSymbolTable)),
+    };
+    symbolStore = new SymbolStore('perf-html-async-storage', symbolProvider);
+  });
+
+  afterEach(async function () {
+    await deleteDatabase();
+  });
+
   it('should only request symbols from the symbol provider once per library', async function () {
-    expect(requestCount).toEqual(0);
+    expect(symbolProvider.requestSymbolTable).not.toHaveBeenCalled();
 
     const lib1 = { debugName: 'firefox', breakpadId: 'dont-care' };
     const addrsForLib1 = await symbolStore.getFuncAddressTableForLib(lib1);
-    expect(requestCount).toEqual(1);
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(1);
     expect(Array.from(addrsForLib1)).toEqual([0, 0xf00, 0x1a00, 0x2000]);
+
     const secondAndThirdSymbol = await symbolStore.getSymbolsForAddressesInLib([1, 2], lib1);
-    expect(requestCount).toEqual(1);
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(1);
     expect(secondAndThirdSymbol).toEqual(['second symbol', 'third symbol']);
 
     const lib2 = { debugName: 'firefox2', breakpadId: 'dont-care2' };
     const addrsForLib2 = await symbolStore.getFuncAddressTableForLib(lib2);
-    expect(requestCount).toEqual(2);
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(2);
     expect(Array.from(addrsForLib2)).toEqual([0, 0xf00, 0x1a00, 0x2000]);
+
     const firstAndLastSymbol = await symbolStore.getSymbolsForAddressesInLib([0, 3], lib2);
-    expect(requestCount).toEqual(2);
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(2);
     expect(firstAndLastSymbol).toEqual(['first symbol', 'last symbol']);
 
     const addrsForLib1AfterTheSecondTime = await symbolStore.getFuncAddressTableForLib(lib1);
-    expect(requestCount).toEqual(2);
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(2);
     expect(addrsForLib1).toEqual(addrsForLib1AfterTheSecondTime);
+  });
+
+  it('should persist in DB', async function () {
+    // Using another symbol store simulates a page reload
+    const symbolStore2 = new SymbolStore('perf-html-async-storage', symbolProvider);
+
+    const lib = { debugName: 'firefox', breakpadId: 'dont-care' };
+    const addrsForLib1 = await symbolStore.getFuncAddressTableForLib(lib);
+    const addrsForLib2 = await symbolStore2.getFuncAddressTableForLib(lib);
+
+    expect(symbolProvider.requestSymbolTable).toHaveBeenCalledTimes(1);
+    expect(addrsForLib2).toEqual(addrsForLib1);
   });
 });
