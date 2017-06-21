25794b325956973a2bf38f4d7a9688e1427bb44b
Greg Tatum <tatum.creative@gmail.com>
2017-06-21 14:54:48 -0500

    Add tests for thread ordering and hiding via the url

--- src/test/url-handling.test.js
+++ src/test/url-handling.test.js
@@ -46,3 +46,49 @@ describe('selectedThread', function () {
     expect(urlStateReducers.getSelectedThreadIndex(store.getState())).toBe(2);
   });
 });
+
+describe('threadOrder and hiddenThreads', function () {
+  const profile: Profile = processProfile(exampleProfile);
+
+  function storeWithSearch(search: string) {
+    const urlState = stateFromLocation({
+      pathname: '/public/1ecd7a421948995171a4bb483b7bcc8e1868cc57/calltree/',
+      search,
+      hash: '',
+    });
+    const store = blankStore();
+    store.dispatch({ type: '@@urlenhancer/updateURLState', urlState });
+    store.dispatch(receiveProfileFromStore(profile));
+    return store;
+  }
+
+  it('decides the threadOrder and hiddenThreads without any set parameters', function () {
+    const { getState } = storeWithSearch('');
+    expect(urlStateReducers.getThreadOrder(getState())).toEqual([0, 2, 1]);
+    expect(urlStateReducers.getHiddenThreads(getState())).toEqual([]);
+  });
+
+  it('can reorder the threads ', function () {
+    const { getState } = storeWithSearch('?threadOrder=1-2-0');
+    expect(urlStateReducers.getThreadOrder(getState())).toEqual([1, 2, 0]);
+    expect(urlStateReducers.getHiddenThreads(getState())).toEqual([]);
+  });
+
+  it('can hide the threads', function () {
+    const { getState } = storeWithSearch('?hiddenThreads=1-2');
+    expect(urlStateReducers.getThreadOrder(getState())).toEqual([0, 2, 1]);
+    expect(urlStateReducers.getHiddenThreads(getState())).toEqual([1, 2]);
+  });
+
+  it('will not accept invalid threads in the thread order', function () {
+    const { getState } = storeWithSearch('?threadOrder=0-8-2-a-1');
+    expect(urlStateReducers.getThreadOrder(getState())).toEqual([0, 2, 1]);
+    expect(urlStateReducers.getHiddenThreads(getState())).toEqual([]);
+  });
+
+  it('will not accept invalid hidden threads', function () {
+    const { getState } = storeWithSearch('?hiddenThreads=0-8-2-a');
+    expect(urlStateReducers.getThreadOrder(getState())).toEqual([0, 2, 1]);
+    expect(urlStateReducers.getHiddenThreads(getState())).toEqual([0, 2]);
+  });
+});
