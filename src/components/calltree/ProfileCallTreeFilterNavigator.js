b3dbc672f69552472ede9e158314072208fe80b0
Michael Brennan <brennan.brisad@gmail.com>
2017-07-21 21:01:28 +0200

    Add flow types to calltree components

--- src/components/calltree/ProfileCallTreeFilterNavigator.js
+++ src/components/calltree/ProfileCallTreeFilterNavigator.js
@@ -2,16 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// @flow
+
 import { connect } from 'react-redux';
 import actions from '../../actions';
 import { selectedThreadSelectors } from '../../reducers/profile-view';
 import { getSelectedThreadIndex } from '../../reducers/url-state';
 import FilterNavigatorBar from './FilterNavigatorBar';
+import type { State } from '../../types/reducers';
 
 import './ProfileCallTreeFilterNavigator.css';
 
 export default connect(
-  state => {
+  (state: State) => {
     const items = selectedThreadSelectors.getCallTreeFilterLabels(state);
     return {
       className: 'profileCallTreeFilterNavigator',
