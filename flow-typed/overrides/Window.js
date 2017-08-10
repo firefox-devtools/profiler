cf6efdc9bff5c580ec20e321e628cf834a5f3e53
Greg Tatum <tatum.creative@gmail.com>
2017-08-10 11:21:56 -0500

    Snapshot test app/Home component

--- flow-typed/overrides/Window.js
+++ flow-typed/overrides/Window.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @flow
 
 import type { IDBFactory } from '../../src/types/indexeddb';
 
@@ -53,6 +54,9 @@ declare class Window extends EventTarget {
   history: History,
   Worker: typeof Worker,
   WheelEvent: WheelEvent,
+  navigator: {
+    userAgent: string,
+  },
 }
 
 declare var window: Window;
