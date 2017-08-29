c8c53792145fdd37d422b12315c9b98fe464806d
Michael Brennan <brennan.brisad@gmail.com>
2017-08-29 15:10:08 +0200

    Add snapshot test for TimelineFlameChart (#552)

---
+++ src/test/components/TimelineFlameChart.test.js
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// @flow
+import React from 'react';
+import TimelineFlameChart from '../../components/timeline/TimelineFlameChart';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import mockCanvasContext from '../fixtures/mocks/canvas-context';
+import { storeWithProfile } from '../fixtures/stores';
+import { getBoundingBox } from '../fixtures/utils';
+import { getProfileForUnfilteredCallTree } from '../fixtures/profiles/profiles-for-call-trees';
+import { changeTimelineFlameChartExpandedThread } from '../../actions/timeline';
+
+jest.useFakeTimers();
+
+it('renders TimelineFlameChart correctly', () => {
+  // Tie the requestAnimationFrame into jest's fake timers.
+  window.requestAnimationFrame = fn => setTimeout(fn, 0);
+  window.devicePixelRatio = 1;
+  const ctx = mockCanvasContext();
+
+  /**
+   * Mock out any created refs for the components with relevant information.
+   */
+  function createNodeMock(element) {
+    // <TimelineCanvas><canvas /></TimelineCanvas>
+    if (element.type === 'canvas') {
+      return {
+        getBoundingClientRect: () => getBoundingBox(200, 300),
+        getContext: () => ctx,
+        style: {},
+      };
+    }
+    // <TimelineViewport />
+    if (element.props.className.split(' ').includes('timelineViewport')) {
+      return {
+        getBoundingClientRect: () => getBoundingBox(200, 300),
+      };
+    }
+    return null;
+  }
+
+  const store = storeWithProfile(getProfileForUnfilteredCallTree());
+  store.dispatch(changeTimelineFlameChartExpandedThread(0, true));
+
+  const timeline = renderer.create(
+    <Provider store={store}>
+      <TimelineFlameChart threadIndex={0} viewHeight={1000} />
+    </Provider>,
+    { createNodeMock }
+  );
+
+  // Flush any requestAnimationFrames.
+  jest.runAllTimers();
+
+  const tree = timeline.toJSON();
+  const drawCalls = ctx.__flushDrawLog();
+
+  expect(tree).toMatchSnapshot();
+  expect(drawCalls).toMatchSnapshot();
+
+  delete window.requestAnimationFrame;
+  delete window.devicePixelRatio;
+});
