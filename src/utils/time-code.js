cf37f3e3f8e5c95545a12da1a9730f87fff51e9e
Greg Tatum <tatum.creative@gmail.com>
2017-10-17 17:16:42 -0500

    Add timing analytics

--- src/utils/time-code.js
+++ src/utils/time-code.js
@@ -4,15 +4,38 @@
 
 // @flow
 
+const MAX_TIMINGS_PER_LABEL = 3;
+const _timingsPerLabel = {};
+
+/**
+ * We care about timing information. This function helps log and collect information
+ * about how fast our functions are.
+ */
 export function timeCode<T>(label: string, codeAsACallback: () => T): T {
-  if (
-    typeof performance !== 'undefined' &&
-    process.env.NODE_ENV === 'development'
-  ) {
+  if (typeof performance !== 'undefined') {
     const start = performance.now();
     const result = codeAsACallback();
     const elapsed = Math.round(performance.now() - start);
-    console.log(`${label} took ${elapsed}ms to execute.`);
+
+    // Only log timing information in development mode.
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`${label} took ${elapsed}ms to execute.`);
+    }
+
+    // Some portion of users will have timing information sent. Limit this further to
+    // only send a few labels per user.
+    const ga = self.ga;
+    if (ga && !(_timingsPerLabel[label] > MAX_TIMINGS_PER_LABEL)) {
+      _timingsPerLabel[label] = 1 + (_timingsPerLabel[label] || 0);
+      ga('send', {
+        hitType: 'timing',
+        timingCategory: 'timeCode',
+        timingVar: label,
+        timingValue: elapsed,
+      });
+    }
+
+    // Return the actual result.
     return result;
   }
   return codeAsACallback();
