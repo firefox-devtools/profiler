e052e079eb172965d633716a9785c1a9d1b7538d
Julien Wajsberg <felash@gmail.com>
2017-11-27 15:23:19 +0100

    Viewport: disable rule react/no-did-update-set-state

--- src/components/shared/chart/Viewport.js
+++ src/components/shared/chart/Viewport.js
@@ -188,7 +188,9 @@ export default function withChartViewport<Props: ViewportProps>(
     }
 
     componentDidUpdate(prevProps: Props) {
+      // TODO Consider merging this in componentWillReceiveProps
       if (this.props.viewportNeedsUpdate(prevProps, this.props)) {
+        // eslint-disable-next-line react/no-did-update-set-state
         this.setState(this.getDefaultState(this.props));
         this._setSizeNextFrame();
       }
