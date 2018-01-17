117cf8cc4a038389d92495c5d02d060cb49907be
Greg Tatum <tatum.creative@gmail.com>
2018-01-17 16:35:54 -0600

    Move libdefs into src/types, and update flow-generate-libdefs to not include stubs
    Fix flow-typed

--- src/types/globals/WheelEvent.js
+++ src/types/globals/WheelEvent.js
@@ -3,11 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 declare class WheelEvent extends MouseEvent {
-  deltaX: number; // readonly
-  deltaY: number; // readonly
-  deltaZ: number; // readonly
-  deltaMode: 0x00 | 0x01 | 0x02; // readonly
-  DOM_DELTA_PIXEL: 0x00; // readonly
-  DOM_DELTA_PAGE: 0x01; // readonly
-  DOM_DELTA_LINE: 0x02; // readonly
+  deltaX: number, // readonly
+  deltaY: number, // readonly
+  deltaZ: number, // readonly
+  deltaMode: 0x00 | 0x01 | 0x02, // readonly
+  DOM_DELTA_PIXEL: 0x00, // readonly
+  DOM_DELTA_PAGE: 0x01, // readonly
+  DOM_DELTA_LINE: 0x02, // readonly
 }
