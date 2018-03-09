e219fe73ec9b5a6c083ba664ea0699918a591083
Greg Tatum <tatum.creative@gmail.com>
2018-03-09 13:00:16 -0600

    Add an enzyme, and a test for ProfileCallTreeContextMenu.js

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,3 +1,8 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 jest.mock('../utils/worker-factory');
 // Disabling eslint for this line because this property is only part of the mock
 import { shutdownWorkers } from '../utils/worker-factory'; // eslint-disable-line import/named
