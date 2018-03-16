65bb73ae5a971cd21c88aeaccb9a5a09f4edb029
Michael Hoffmann <brennan.brisad@gmail.com>
2018-03-16 17:48:22 +0100

    Flame graph colors (#809)
    * Return selfTime and selfTimeRelative from CallTree.getNodeData
    
    * Add selfTimeRelative and display fields to FlameGraphTiming type
    
    * Add tooltip details to flame graph tooltips
    
    Show implementation, running time and self time in flame graph
    tooltips.
    
    * Add colors to the flame graph
    
    * Improve flame graph colors after code review
    
    - Use photon colors
    - Change Implementation type to StackType
    - Add unit test
    - Provide more documentation
    - Use switch statements to better catch errors
    
    * Reorder imports
    
    * Address review comments for flame graph colors
    
    - Change to lighter colors, making it easier on the eyes.
    - Remove dependency on d3-interpolate and implement only what we
      need. This greatly improves performance.
    - Make changes to avoid allocations in hot code.
    
    * Update selector snapshot test for FlameGraphTiming
    
    * Fix long form comment format

--- src/profile-logic/flame-graph.js
+++ src/profile-logic/flame-graph.js
@@ -11,9 +11,30 @@ import * as CallTree from './call-tree';
 export type FlameGraphDepth = number;
 export type IndexIntoFlameGraphTiming = number;
 
+/**
+ * FlameGraphTiming is an array containing data used for rendering the
+ * flame graph. Each element in the array describes one row in the
+ * graph. Each such element in turn contains one or more functions,
+ * drawn as boxes with start and end positions, represented as unit
+ * intervals of the profile range. It should be noted that start and
+ * end does not represent units of time, but only positions on the
+ * x-axis, derived from an alphabetical sort.
+ *
+ * callNode allows extracting information such as function names which
+ * are shown in the flame graph.
+ *
+ * selfTimeRelative contains the self time relative to the total time,
+ * which is used to color the drawn functions.
+ *
+ * display contains formatted strings intended to be used for
+ * displaying extra information (like in a tooltip) not readily
+ * apparent from visual inspection of the boxes in the flame graph.
+ */
 export type FlameGraphTiming = Array<{
   start: UnitIntervalOfProfileRange[],
   end: UnitIntervalOfProfileRange[],
+  selfTimeRelative: Array<number>,
+  display: Array<{ totalTime: string, selfTime: string }>,
   callNode: IndexIntoCallNodeTable[],
   length: number,
 }>;
@@ -41,7 +62,11 @@ export function getFlameGraphTiming(
 
   while (stack.length) {
     const { depth, nodeIndex } = stack.pop();
-    const { totalTimeRelative } = callTree.getNodeData(nodeIndex);
+    const { totalTimeRelative, selfTimeRelative } = callTree.getNodeData(
+      nodeIndex
+    );
+
+    const { totalTime, selfTime } = callTree.getDisplayData(nodeIndex);
 
     // Select an existing row, or create a new one.
     let row = timing[depth];
@@ -49,6 +74,8 @@ export function getFlameGraphTiming(
       row = {
         start: [],
         end: [],
+        selfTimeRelative: [],
+        display: [],
         callNode: [],
         length: 0,
       };
@@ -58,6 +85,8 @@ export function getFlameGraphTiming(
     // Compute the timing information.
     row.start.push(timeOffset[depth]);
     row.end.push(timeOffset[depth] + totalTimeRelative);
+    row.selfTimeRelative.push(selfTimeRelative);
+    row.display.push({ totalTime, selfTime });
     row.callNode.push(nodeIndex);
     row.length++;
 
