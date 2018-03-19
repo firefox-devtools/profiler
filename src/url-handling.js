dbaf153592a5aaafdcf784292cd0ea33e97a7565
Julien Wajsberg <felash@gmail.com>
2018-03-19 11:18:07 +0100

    Fix prettier

--- src/url-handling.js
+++ src/url-handling.js
@@ -93,7 +93,7 @@ export function urlStateToUrlObject(urlState: UrlState): UrlObject {
     file: urlState.pathInZipFile || undefined,
     v: CURRENT_URL_VERSION,
   };
-  
+
   // Add the parameter hiddenThreads only when needed
   if (urlState.hiddenThreads.length > 0) {
     query.hiddenThreads = urlState.hiddenThreads.join('-');
