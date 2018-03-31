8fcac8a9421d1fcd509a307d89af7e58a3290b3c
Diesel-Gaming <jwedley@msudenver.edu>
2018-03-31 16:05:04 -0600

    moved conditional statement and changed behavior from displaying "Only Show:..." to always showing menu item and setting disabled to true or false from conditional

--- src/components/header/ProfileThreadHeaderContextMenu.js
+++ src/components/header/ProfileThreadHeaderContextMenu.js
@@ -85,14 +85,19 @@ class ProfileThreadHeaderContextMenu extends PureComponent<Props> {
 
     return (
       <ContextMenu id={'ProfileThreadHeaderContextMenu'}>
-        {hiddenThreads.length === threads.length - 1 ? null : (
+        {
           <div>
-            <MenuItem onClick={this._isolateThread}>
+            <MenuItem
+              onClick={this._isolateThread}
+              disabled={
+                hiddenThreads.length === threads.length - 1 ? true : false
+              }
+            >
               Only show: {`"${clickedThreadName}"`}
             </MenuItem>
             <div className="react-contextmenu-separator" />
           </div>
-        )}
+        }
         {threadOrder.map(threadIndex => {
           const isHidden = hiddenThreads.includes(threadIndex);
           return (
