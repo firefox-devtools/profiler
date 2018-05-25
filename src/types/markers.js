4015ef9f0ba53e573fc3f1a27dc1c1ff7dcccde3
Nazım Can Altınova <canaltinova@gmail.com>
2018-05-25 15:56:50 +0200

    Add new profile data to test and change some tests after profile data change

--- src/types/markers.js
+++ src/types/markers.js
@@ -312,6 +312,7 @@ export type DOMEventMarkerPayload = {
   interval: 'start' | 'end',
   eventType: string,
   phase: 0 | 1 | 2 | 3,
+  cause?: CauseBacktrace,
 };
 
 type StyleMarkerPayload_Shared = {
