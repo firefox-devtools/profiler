ec6bc7e18349784d2e9f16e0094dabe3711cf31b
Greg Tatum <tatum.creative@gmail.com>
2018-07-03 13:51:40 -0500

    Switch from a thread-based timeline to a track-based one; r=julienw

--- src/types/reducers.js
+++ src/types/reducers.js
@@ -13,8 +13,18 @@ import type {
 } from './actions';
 import type { TabSlug } from '../app-logic/tabs-handling';
 import type { Milliseconds, StartEndRange } from './units';
-import type { IndexIntoMarkersTable, Profile, ThreadIndex } from './profile';
-import type { CallNodePath } from './profile-derived';
+import type {
+  IndexIntoMarkersTable,
+  Profile,
+  ThreadIndex,
+  Pid,
+} from './profile';
+import type {
+  CallNodePath,
+  GlobalTrack,
+  LocalTrack,
+  TrackIndex,
+} from './profile-derived';
 import type { Attempt } from '../utils/errors';
 import type { GetLabel } from '../profile-logic/labeling-strategies';
 import type { GetCategory } from '../profile-logic/color-categories';
@@ -52,6 +62,8 @@ export type ProfileViewState = {
     isCallNodeContextMenuVisible: boolean,
     profileSharingStatus: ProfileSharingStatus,
   },
+  globalTracks: GlobalTrack[],
+  localTracksByPid: Map<Pid, LocalTrack[]>,
   profile: Profile | null,
 };
 
@@ -124,13 +136,15 @@ export type UrlState = {|
   selectedTab: TabSlug,
   pathInZipFile: string | null,
   profileSpecific: {|
+    selectedThread: ThreadIndex | null,
+    globalTrackOrder: TrackIndex[],
+    hiddenGlobalTracks: Set<TrackIndex>,
+    hiddenLocalTracksByPid: Map<Pid, Set<TrackIndex>>,
+    localTrackOrderByPid: Map<Pid, TrackIndex[]>,
     implementation: ImplementationFilter,
     invertCallstack: boolean,
     rangeFilters: StartEndRange[],
-    selectedThread: ThreadIndex | null,
     callTreeSearchString: string,
-    threadOrder: ThreadIndex[],
-    hiddenThreads: ThreadIndex[],
     markersSearchString: string,
     transforms: TransformStacksPerThread,
   |},
