d30c5bbf01ea5e4f476e61af76021139e8da7736
saranshbarua <saranshbarua@gmail.com>
2018-09-19 00:24:23 +0530

    fix browser's back button

--- src/components/app/Root.js
+++ src/components/app/Root.js
@@ -185,17 +185,22 @@ class ProfileViewWhenReadyImpl extends PureComponent<ProfileViewProps> {
         return this.renderMessage(message, additionalMessage, showLoader);
       }
       case 'FATAL_ERROR': {
-        const message =
-          ERROR_MESSAGES[dataSource] || "Couldn't retrieve the profile.";
-        let additionalMessage = null;
-        if (view.error) {
-          console.error(view.error);
-          additionalMessage =
-            `${view.error.toString()}\n` +
-            'The full stack has been written to the Web Console.';
+
+        if(dataSource == 'none') {
+          return <Home />
         }
 
-        return this.renderMessage(message, additionalMessage, false);
+        else {
+          const message = ERROR_MESSAGES[dataSource] || "Couldn't retrieve the profile.";
+          let additionalMessage = null;
+          if (view.error) {
+            console.error(view.error);
+            additionalMessage =
+              `${view.error.toString()}\n` +
+              'The full stack has been written to the Web Console.';
+          }
+            return this.renderMessage(message, additionalMessage, false);
+        }
       }
       case 'DATA_LOADED':
         // The data is now loaded. This could be either a single profile, or a zip file
