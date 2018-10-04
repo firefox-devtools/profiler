d9bbd551b5d7f301657306f470503f3dc972ba06
Ola Gasidlo <o.gasidlo@gmail.com>
2018-10-04 12:19:07 +0200

    Fixes flow errors + aligns chart
    * Fix flow errors for the NetworkChart PR #1232
    
    * Properly align the network panel rows
    
    This commit changes the implementation from using the screen sizing, to
    measuring the component with the WithSize higher order component. It
    removed some CSS margins and padding that was getting in the way of the
    layout as well.
    
    Some of the methods were broken out, due to the VirtualListRow not
    updating when the props were changing. I added a memoized function that
    accepts the current props, and generates an array of the
    NetworkChartRowProps that will properly trigger the VirtualListRow to
    re-render. This update removed the React index values, as that is
    handled by the VirtualListRow.
    
    * Update snapshots for the network chart

--- src/components/network-chart/NetworkChartRow.js
+++ src/components/network-chart/NetworkChartRow.js
@@ -10,14 +10,16 @@ import Tooltip from '../shared/Tooltip';
 import type { CssPixels } from '../../types/units';
 import type { ThreadIndex } from '../../types/profile';
 import type { TracingMarker } from '../../types/profile-derived';
-import type { MarkerPayload } from '../../types/markers';
+import type { NetworkPayload } from '../../types/markers';
 
-type Props = {
+export type NetworkChartRowProps = {
   +marker: TracingMarker,
+  // Pass the payload in as well, since our types can't express a TracingMarker with
+  // a specific payload.
+  +networkPayload: NetworkPayload | null,
   +markerStyle: {
     [key: string]: string | number,
   },
-  +index: number,
   +threadIndex: ThreadIndex,
 };
 
@@ -27,7 +29,7 @@ type State = {
   hovered: ?boolean,
 };
 
-class NetworkChartRow extends React.PureComponent<Props, State> {
+class NetworkChartRow extends React.PureComponent<NetworkChartRowProps, State> {
   state = {
     pageX: 0,
     pageY: 0,
@@ -51,45 +53,35 @@ class NetworkChartRow extends React.PureComponent<Props, State> {
     });
   };
 
-  _getMarkerData(marker: TracingMarker): ?MarkerPayload {
-    if (marker.data === undefined && marker.data === null) {
-      console.error('Network marker has no data!');
+  render() {
+    const { marker, markerStyle, networkPayload } = this.props;
+
+    if (networkPayload === null) {
       return null;
     }
-    return marker.data;
-  }
+    const itemClassName = ('item ' + networkPayload.status).toLowerCase();
 
-  render() {
-    const { marker, markerStyle } = this.props;
-
-    const markerData = this._getMarkerData(marker);
-
-    if (markerData !== null) {
-      const itemClassName = ('item ' + markerData.status).toLowerCase();
-
-      return (
-        <section className={itemClassName}>
-          <div className="itemLabel">{marker.name}</div>
-          <div
-            className="itemBar"
-            style={markerStyle}
-            onMouseEnter={this._hoverIn}
-            onMouseLeave={this._hoverOut}
-          >
-            &nbsp;
-          </div>
-          {this.state.hovered ? (
-            <Tooltip mouseX={this.state.pageX} mouseY={this.state.pageY}>
-              <MarkerTooltipContents
-                marker={marker}
-                threadIndex={this.props.threadIndex}
-              />
-            </Tooltip>
-          ) : null}
-        </section>
-      );
-    }
-    return null;
+    return (
+      <section className={itemClassName}>
+        <div className="itemLabel">{marker.name}</div>
+        <div
+          className="itemBar"
+          style={markerStyle}
+          onMouseEnter={this._hoverIn}
+          onMouseLeave={this._hoverOut}
+        >
+          &nbsp;
+        </div>
+        {this.state.hovered ? (
+          <Tooltip mouseX={this.state.pageX} mouseY={this.state.pageY}>
+            <MarkerTooltipContents
+              marker={marker}
+              threadIndex={this.props.threadIndex}
+            />
+          </Tooltip>
+        ) : null}
+      </section>
+    );
   }
 }
 
--- src/components/network-chart/index.js
+++ src/components/network-chart/index.js
@@ -3,12 +3,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // @flow
+import clamp from 'clamp';
+import { oneLine } from 'common-tags';
 import * as React from 'react';
+import {
+  TIMELINE_MARGIN_LEFT,
+  TIMELINE_MARGIN_RIGHT,
+} from '../../app-logic/constants';
 import explicitConnect from '../../utils/connect';
 import MarkerSettings from '../shared/MarkerSettings';
 import VirtualList from '../shared/VirtualList';
+import { withSize } from '../shared/WithSize';
 import NetworkChartEmptyReasons from './NetworkChartEmptyReasons';
 import NetworkChartRow from './NetworkChartRow';
+import memoize from 'memoize-immutable';
+import MixedTupleMap from 'mixedtuplemap';
 
 import {
   selectedThreadSelectors,
@@ -18,6 +27,8 @@ import {
 import { getSelectedThreadIndex } from '../../reducers/url-state';
 import { updatePreviewSelection } from '../../actions/profile-view';
 
+import type { SizeProps } from '../shared/WithSize';
+import type { NetworkPayload } from '../../types/markers';
 import type {
   TracingMarker,
   MarkerTimingRows,
@@ -25,16 +36,20 @@ import type {
 import type {
   Milliseconds,
   UnitIntervalOfProfileRange,
+  CssPixels,
 } from '../../types/units';
 import type {
   ExplicitConnectOptions,
   ConnectedProps,
 } from '../../utils/connect';
+import type { NetworkChartRowProps } from './NetworkChartRow';
 
 require('./index.css');
 
 const ROW_HEIGHT = 17.5;
-const CONTAINER_WIDTH = 150 / window.screen.width * 100; // this will be replaced by the viewport
+
+// The SizeProps are injected by the WithSize higher order component.
+type OwnProps = SizeProps;
 
 type DispatchProps = {|
   +updatePreviewSelection: typeof updatePreviewSelection,
@@ -49,7 +64,17 @@ type StateProps = {|
   +threadIndex: number,
 |};
 
-type Props = ConnectedProps<{||}, StateProps, DispatchProps>;
+type Props = ConnectedProps<OwnProps, StateProps, DispatchProps>;
+
+/*
+ * The VirtualListRows only re-render when their items have changed. This information
+ * is derived from the current props, which includes time range, container sizing,
+ * and the marker information itself. In order to properly update the rows,
+ * provide a memoized function that can compute this on the fly.
+ */
+const _getVirtualListItemsMemoized = memoize(_getVirtualListItems, {
+  cache: new MixedTupleMap(),
+});
 
 class NetworkChart extends React.PureComponent<Props> {
   /**
@@ -60,67 +85,16 @@ class NetworkChart extends React.PureComponent<Props> {
     return interval / (end - start);
   }
 
-  _getMarkerPosition = markerStart => {
-    const timeRange = this.props.timeRange;
-    const timeRangeTotal = timeRange.end - timeRange.start;
-
-    let markerPosition =
-      (markerStart - timeRange.start) *
-      (100 - CONTAINER_WIDTH) /
-      timeRangeTotal;
-
-    // the bar shall not overlap the first 150px as this is the thread label area.
-    if (markerPosition < 0) {
-      markerPosition = 0;
-    }
-
-    markerPosition = markerPosition + CONTAINER_WIDTH;
-
-    return markerPosition;
+  _onCopy = (_event: Event) => {
+    // No implemented.
   };
 
-  _getMarkerLength = markerDuration => {
-    const timeRange = this.props.timeRange;
-    const timeRangeTotal = timeRange.end - timeRange.start;
-
-    let markerLength =
-      markerDuration * (100 - CONTAINER_WIDTH) / timeRangeTotal;
-
-    if (markerLength < 0.1) {
-      markerLength = 0.1;
-    }
-
-    return markerLength;
-  };
-
-  _getMarkerStyling = (marker: TracingMarker) => {
-    const markerLength = this._getMarkerLength(marker.dur);
-    const markerPosition = this._getMarkerPosition(marker.data.startTime);
-
-    const markerStyling = {
-      width: markerLength + '%',
-      left: markerPosition + '%',
-    };
-    return markerStyling;
-  };
-
-  // Create row with correct details
-  _renderRow = (nodeId: any, index: number) => {
-    const marker = this.props.markers;
-
-    return (
-      <NetworkChartRow
-        marker={marker[index]}
-        index={index}
-        markerStyle={this._getMarkerStyling(marker[index])}
-        threadIndex={this.props.threadIndex}
-      />
-    );
+  _onKeyDown = (_event: KeyboardEvent) => {
+    // No implemented.
   };
 
   render() {
     const { markers } = this.props;
-
     return (
       <div className="networkChart">
         <MarkerSettings />
@@ -129,14 +103,16 @@ class NetworkChart extends React.PureComponent<Props> {
         ) : (
           <VirtualList
             className="treeViewBody"
-            items={markers}
-            renderItem={this._renderRow}
+            items={_getVirtualListItemsMemoized(this.props)}
+            renderItem={_renderRow}
             itemHeight={ROW_HEIGHT}
             columnCount={1}
             focusable={true}
             specialItems={[]}
             containerWidth={3000}
             disableOverscan={true}
+            onCopy={this._onCopy}
+            onKeyDown={this._onKeyDown}
           />
         )}
       </div>
@@ -144,7 +120,7 @@ class NetworkChart extends React.PureComponent<Props> {
   }
 }
 
-const options: ExplicitConnectOptions<{||}, StateProps, DispatchProps> = {
+const options: ExplicitConnectOptions<OwnProps, StateProps, DispatchProps> = {
   mapStateToProps: state => {
     const networkTimingRows = selectedThreadSelectors.getNetworkChartTiming(
       state
@@ -160,4 +136,92 @@ const options: ExplicitConnectOptions<{||}, StateProps, DispatchProps> = {
   },
   component: NetworkChart,
 };
-export default explicitConnect(options);
+
+/**
+ * Wrap the component in the WithSize higher order component, as well as the redux
+ * connected component.
+ */
+export default withSize(explicitConnect(options));
+
+/**
+ * The VirtualListRow only re-renders when the props change, so pass in a pure function
+ * rather than a method, so it is clear to not use `this.props`, as this would bypass
+ * the update cycle, and rows would not be correctly re-rendered.
+ */
+function _renderRow(rowProps: NetworkChartRowProps): React.Node {
+  return <NetworkChartRow {...rowProps} />;
+}
+
+/**
+ * Our definition of tracing markers does not currently have the ability to refine
+ * the union of all payloads to one specific payload through the type definition.
+ * This function does a runtime check to do so.
+ */
+function _getNetworkPayloadOrNull(
+  marker: TracingMarker
+): null | NetworkPayload {
+  if (!marker.data || marker.data.type !== 'Network') {
+    return null;
+  }
+  return marker.data;
+}
+
+/**
+ * Convert the time for a network marker into the CssPixels to be used on the screen.
+ * This function takes into account the range used, as well as the container sizing
+ * as passed in by the WithSize component.
+ */
+function _timeToCssPixels(props: Props, time: Milliseconds): CssPixels {
+  const { timeRange, width } = props;
+  const timeRangeTotal = timeRange.end - timeRange.start;
+  const innerContainerWidth =
+    width - TIMELINE_MARGIN_LEFT - TIMELINE_MARGIN_RIGHT;
+
+  const markerPosition =
+    (time - timeRange.start) / timeRangeTotal * innerContainerWidth +
+    TIMELINE_MARGIN_LEFT;
+
+  // Keep the value bounded to the available viewport area.
+  return clamp(markerPosition, 0, width);
+}
+
+/**
+ * Compute the NetworkChartRowProps for each marker. See _getVirtualListItemsMemoized
+ * for more information.
+ */
+function _getVirtualListItems(props: Props): NetworkChartRowProps[] {
+  const { markers, threadIndex } = props;
+  return markers.map(marker => {
+    // Since our type definition for TracingMarker can't refine to just Network
+    // markers, extract the payload.
+    const networkPayload = _getNetworkPayloadOrNull(marker);
+    if (networkPayload === null) {
+      console.error(
+        oneLine`
+          The NetworkChart is supposed to only receive Network markers, but some other
+          kind of marker payload was passed in.
+        `
+      );
+      return {
+        marker,
+        threadIndex,
+        networkPayload: null,
+        markerStyle: {},
+      };
+    }
+
+    // Compute the positioning of the network markers.
+    const startPosition = _timeToCssPixels(props, networkPayload.startTime);
+    const endPosition = _timeToCssPixels(props, networkPayload.endTime);
+
+    return {
+      marker,
+      networkPayload,
+      threadIndex,
+      markerStyle: {
+        left: startPosition,
+        width: endPosition - startPosition,
+      },
+    };
+  });
+}
