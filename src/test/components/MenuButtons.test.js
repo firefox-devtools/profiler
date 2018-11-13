819272d64c4c0064b730ffd7d08457bee91fa9d2
Nazım Can Altınova <canaltinova@gmail.com>
2018-11-13 15:59:56 +0100

    Add a setup function to centralize the rendering

--- src/test/components/MenuButtons.test.js
+++ src/test/components/MenuButtons.test.js
@@ -16,16 +16,24 @@ import { createGeckoProfileWithJsTimings } from '../fixtures/profiles/gecko-prof
 import { processProfile } from '../../profile-logic/process-profile';
 
 describe('app/MenuButtons', function() {
-  // profile.meta.networkURLsRemoved flag is set to false as a default.
-  it('renders the MenuButtons buttons', () => {
-    const store = storeWithProfile();
-    store.dispatch(startSymbolicating());
-
+  function setup(profile) {
+    const store = storeWithProfile(profile);
     const { container } = render(
       <Provider store={store}>
         <MenuButtons />
       </Provider>
     );
+
+    return {
+      store,
+      container,
+    };
+  }
+
+  // profile.meta.networkURLsRemoved flag is set to false as a default.
+  it('renders the MenuButtons buttons', () => {
+    const { store, container } = setup();
+    store.dispatch(startSymbolicating());
 
     // MenuButtons is rendering a fragment with several children. We need to
     // check all children to assess that the component renders properly.
@@ -38,14 +46,8 @@ describe('app/MenuButtons', function() {
   it('renders the MenuButtons buttons with profile.meta.networkURLsRemoved set to true', () => {
     const profile = processProfile(createGeckoProfileWithJsTimings());
     profile.meta.networkURLsRemoved = true;
-    const store = storeWithProfile(profile);
+    const { store, container } = setup(profile);
     store.dispatch(startSymbolicating());
-
-    const { container } = render(
-      <Provider store={store}>
-        <MenuButtons />
-      </Provider>
-    );
 
     expect(Array.from(container.children)).toMatchSnapshot();
 
@@ -56,14 +58,8 @@ describe('app/MenuButtons', function() {
   it('renders the MenuButtons buttons with profile.meta.networkURLsRemoved set to undefined', () => {
     const profile = processProfile(createGeckoProfileWithJsTimings());
     profile.meta.networkURLsRemoved = undefined;
-    const store = storeWithProfile(profile);
+    const { store, container } = setup(profile);
     store.dispatch(startSymbolicating());
-
-    const { container } = render(
-      <Provider store={store}>
-        <MenuButtons />
-      </Provider>
-    );
 
     expect(Array.from(container.children)).toMatchSnapshot();
 
