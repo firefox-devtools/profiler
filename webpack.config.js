f554012faf865c20b773d5837a1279a0a3de4d95
hashi93 <hashigalappaththi1993@gmail.com>
2018-11-20 21:13:36 +0530

    UglifyjsWebpackPlugin added

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const includes = [path.join(__dirname, 'src'), path.join(__dirname, 'res')];
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const es6modules = ['pretty-bytes'];
 const es6modulePaths = es6modules.map(module => {
@@ -96,7 +97,13 @@ const config = {
   optimization: {
     // Workaround for https://github.com/webpack/webpack/issues/7760
     usedExports: false,
-    minimizer: [new OptimizeCSSAssetsPlugin({})],
+    minimizer: [
+        new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true
+        }),
+        new OptimizeCSSAssetsPlugin({})],
   },
 };
 
