648a5fad6d85e8d380a1b4f375e2106c85a35ef3
Julien Wajsberg <felash@gmail.com>
2018-11-21 20:13:31 +0100

    Add the View360 component

---
+++ src/components/view-360/index.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// @flow
+
+import React from 'react';
+
+import './View360.css';
+
+export default function View360(_props: {||}) {
+  return (
+    <section className="view360">
+      <div className="view360-overview" />
+      <div className="view360-details" />
+    </section>
+  );
+}
