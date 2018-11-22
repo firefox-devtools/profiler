2317f524da13a22c516e0d16962b8053eb387ce0
Julien Wajsberg <felash@gmail.com>
2018-11-22 18:17:55 +0100

    Better style and text polish

--- src/components/app/ServiceWorkerManager.js
+++ src/components/app/ServiceWorkerManager.js
@@ -16,6 +16,8 @@ import type {
 import type { DataSource } from '../../types/actions';
 import type { Phase } from '../../types/reducers';
 
+import './ServiceWorkerManager.css';
+
 type StateProps = {|
   +dataSource: DataSource,
   +phase: Phase,
@@ -25,10 +27,14 @@ type Props = ConnectedProps<{||}, StateProps, {||}>;
 type InstallStatus = 'pending' | 'ready' | 'idle';
 type State = {|
   installStatus: InstallStatus,
+  closeNotice: boolean,
 |};
 
 class ServiceWorkerManager extends PureComponent<Props, State> {
-  state = { installStatus: 'idle' };
+  state = {
+    installStatus: 'idle',
+    closeNotice: true,
+  };
 
   _installServiceWorker() {
     // We use '@mstange/offline-plugin/runtime' here instead of
@@ -53,13 +59,13 @@ class ServiceWorkerManager extends PureComponent<Props, State> {
       },
       onUpdated: () => {
         console.log(
-          '[ServiceWorker] The new version of the service worker has been enabled.'
+          '[ServiceWorker] The new version of the application has been enabled.'
         );
-        this.setState({ installStatus: 'ready' });
+        this.setState({ installStatus: 'ready', closeNotice: false });
       },
       onUpdateFailed: () => {
         console.log(
-          '[ServiceWorker] We failed to update the service worker for an unknown reason.'
+          '[ServiceWorker] We failed to update the application for an unknown reason.'
         );
       },
     });
@@ -80,15 +86,23 @@ class ServiceWorkerManager extends PureComponent<Props, State> {
     }
 
     if (installStatus === 'ready') {
-      window.location.reload();
+      this.reloadPage();
     }
   }
 
+  reloadPage() {
+    window.location.reload();
+  }
+
+  _onCloseNotice = () => {
+    this.setState({ closeNotice: true });
+  };
+
   render() {
     const { dataSource } = this.props;
-    const { installStatus } = this.state;
+    const { installStatus, closeNotice } = this.state;
 
-    if (dataSource !== 'none' && dataSource === 'public') {
+    if (dataSource !== 'none' && dataSource !== 'public') {
       return null;
     }
 
@@ -96,12 +110,33 @@ class ServiceWorkerManager extends PureComponent<Props, State> {
       return null;
     }
 
+    if (closeNotice) {
+      return null;
+    }
+
     return (
-      <div className="serviceworker-ready-notice">
-        An update is ready, if you want to use it now you can
-        <a href="javascript:location.reload()">
-          click here to reload the page
-        </a>.
+      <div className="serviceworker-ready-notice-wrapper">
+        {/* We use the wrapper to horizontally center the notice */}
+        <div className="serviceworker-ready-notice">
+          <p>
+            A new version of the application has been downloaded and is ready to
+            use.
+          </p>
+          <button
+            className="photon-button serviceworker-ready-notice-action-button"
+            type="button"
+            onClick={this.reloadPage}
+          >
+            Reload the application
+          </button>
+          <button
+            aria-label="Hide the reload notice"
+            title="Hide the reload notice"
+            className="photon-button photon-button-ghost serviceworker-ready-notice-close-button"
+            type="button"
+            onClick={this._onCloseNotice}
+          />
+        </div>
       </div>
     );
   }
