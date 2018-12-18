0e55aef7106bc21a2cb53f9f2be7fd492a119f58
EVA <edie.blues@gmail.com>
2018-12-18 10:28:04 +0000

    fixes tests for TabBar component

--- src/test/components/TabBar.test.js
+++ src/test/components/TabBar.test.js
@@ -8,29 +8,29 @@ import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
 import TabBar from '../../components/app/TabBar';
 
-afterEach(cleanup);
+describe('app/TabBar', () => {
+  afterEach(cleanup);
 
-it('renders the TabBar', () => {
-  const myMock = jest.fn();
-  render(
-    <TabBar
-      selectedTabSlug="flame-graph"
-      visibleTabs={[
-        'calltree',
-        'flame-graph',
-        'stack-chart',
-        'marker-chart',
-        'marker-table',
-        'network-chart',
-      ]}
-      onSelectTab={myMock}
-    />
-  );
-});
+  it('renders the TabBar and handles clicks properly', () => {
+    const handleTabSelection = jest.fn();
+    const { getByText, container } = render(
+      <TabBar
+        selectedTabSlug="flame-graph"
+        visibleTabs={[
+          'calltree',
+          'flame-graph',
+          'stack-chart',
+          'marker-chart',
+          'marker-table',
+          'network-chart',
+        ]}
+        onSelectTab={handleTabSelection}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
 
-it('calls onClick prop on tab click or enter key press', () => {
-  const onClick = jest.fn();
-  const { getByText } = render(<li onClick={onClick}>Stack Chart</li>);
-  fireEvent.click(getByText('Stack Chart'));
-  expect(onClick).toHaveBeenCalledTimes(1);
+    const leftClick = { button: 0 };
+    fireEvent.click(getByText('Call Tree'), leftClick);
+    expect(handleTabSelection).toHaveBeenCalledWith('calltree');
+  });
 });
