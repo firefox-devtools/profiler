gregtatum/perf.html

Add disk io markers

src/profile-logic/marker-data.js
@@ -357,6 +357,10 @@ export function isNetworkMarker(marker: Marker): boolean {
   return !!(marker.data && marker.data.type === 'Network');
 }
 
+export function isDiskIoMarker(marker: Marker): boolean {
+  return !!(marker.data && marker.data.type === 'io');
+}
+
 export function filterForNetworkChart(markers: Marker[]) {
   return markers.filter(marker => isNetworkMarker(marker));
 }

firefox-devtools/profiler

src/profile-logic/marker-data.js
@@ -413,6 +413,10 @@ export function isNavigationMarker({ name, data }: Marker) {
   return false;
 }
 
+export function isDiskIoMarker(marker: Marker): boolean {
+  return !!(marker.data && marker.data.type === 'DiskIO');
+}
+
 export function filterForNetworkChart(markers: Marker[]) {
   return markers.filter(marker => isNetworkMarker(marker));
 }