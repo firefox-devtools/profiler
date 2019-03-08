daef87608ef19461c60233fd20fe8e86c569adf9
Greg Tatum <tatum.creative@gmail.com>
2019-03-08 08:38:29 -0600

    Use webpack-dev-server for photon components

--- res/photon/server.js
+++ res/photon/server.js
@@ -1,24 +1,26 @@
 // @noflow
-const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const app = express();
+const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config.js');
-const compiler = webpack(config);
+const webpack = require('webpack');
 
 const port = process.env.FX_PROFILER_PORT || 4242;
 const host = process.env.FX_PROFILER_HOST || 'localhost';
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+const serverConfig = {
+  allowedHosts: ['localhost', '.gitpod.io'],
+  contentBase: config.output.path,
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true,
+  },
+};
 
-app.listen(port, host, function() {
+new WebpackDevServer(webpack(config), serverConfig).listen(port, host, function(
+  err
+) {
+  if (err) {
+    console.log(err);
+  }
   console.log(
     `> Photon styling is listening at: http://${host}:${port}/photon/\n`
   );
