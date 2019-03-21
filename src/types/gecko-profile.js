e77e8e891a66c88478d1bdedafdf390f5c759a0f
Julien Wajsberg <felash@gmail.com>
2019-03-21 15:19:40 +0100

    Update src/types/gecko-profile.js
    Co-Authored-By: canaltinova <canaltinova@users.noreply.github.com>

--- src/types/gecko-profile.js
+++ src/types/gecko-profile.js
@@ -195,7 +195,7 @@ export type GeckoProfileMeta = {|
   // This value represents a boolean, but for some reason is written out as an int
   // value as the previous field.
   // It's 0 for opt builds, and 1 for debug builds.
-  // This property is added to Firefox Profiler long time after it's been added to
+  // This property was added to Firefox Profiler a long time after it was added to
   // Firefox, that's why we don't need to make it optional for gecko profiles.
   debug: 0 | 1,
   toolkit: string,
