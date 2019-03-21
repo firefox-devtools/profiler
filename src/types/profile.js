7ec7d885d0fa3307b6fbec41e14c0f20ceeaeb60
Julien Wajsberg <felash@gmail.com>
2019-03-21 15:19:50 +0100

    Update src/types/profile.js
    Co-Authored-By: canaltinova <canaltinova@users.noreply.github.com>

--- src/types/profile.js
+++ src/types/profile.js
@@ -330,7 +330,7 @@ export type ProfileMeta = {|
   // It's 0 for the stack walking feature being turned off, and 1 for stackwalking being
   // turned on.
   stackwalk: 0 | 1,
-  // A boolean flag indicating whether the application is debug build.
+  // A boolean flag indicating whether the profiled application is using a debug build.
   // It's false for opt builds, and true for debug builds.
   // This property is optional because older processed profiles don't have this but
   // this property was added to Firefox long time ago. It should work on older Firefox
