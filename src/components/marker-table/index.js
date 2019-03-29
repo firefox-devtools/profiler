1f1226cc5e3c830770216bd5ff1da6db378cd62a
Pierre Depaz <pierre.depaz@gmail.com>
2019-03-29 11:16:36 +0100

    Added dash separator for Text markers (PR #1906)
    Fixes #1902

--- src/components/marker-table/index.js
+++ src/components/marker-table/index.js
@@ -130,7 +130,7 @@ class MarkerTree {
             break;
           case 'Text':
             category = 'Text';
-            name += ` [${data.name}]`;
+            name += ` — ${data.name}`;
             break;
           default:
         }
