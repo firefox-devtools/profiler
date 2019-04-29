6e2d1e8714c1c7eea5d75f846aaa42c1a5ab99db
Thomas White <im.toms.inbox@gmail.com>
2019-04-29 12:03:13 +1000

    Added test for shared context menu component

---
+++ src/test/components/ContextMenu.test.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// @flow
+import * as React from 'react';
+import { render } from 'react-testing-library';
+import ContextMenu from '../../components/shared/ContextMenu';
+
+describe('ContextMenu', function() {
+  it('correctly renders the context-menu with the props that were passed through', () => {
+    const component = render(
+      <ContextMenu
+        id="contextMenu"
+        className="context-menu"
+        data={{}}
+        onShow={() => null}
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
