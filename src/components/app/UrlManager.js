0751cc2a935ee653755fd739a926edee1dd6c1ba
Nazım Can Altınova <canaltinova@gmail.com>
2019-07-02 15:43:54 +0200

    Remove the $FlowFixMe by using WrapFunctionInDispatch

--- src/components/app/UrlManager.js
+++ src/components/app/UrlManager.js
@@ -19,11 +19,17 @@ import {
   stateFromLocation,
   getIsHistoryReplaceState,
 } from '../../app-logic/url-handling';
-import { getProfilesFromRawUrl } from '../../actions/receive-profile';
+import {
+  getProfilesFromRawUrl,
+  typeof getProfilesFromRawUrl as GetProfilesFromRawUrl,
+} from '../../actions/receive-profile';
 import { ProfileLoaderAnimation } from './ProfileLoaderAnimation';
 import { assertExhaustiveCheck } from '../../utils/flow';
 
-import type { ConnectedProps } from '../../utils/connect';
+import type {
+  ConnectedProps,
+  WrapFunctionInDispatch,
+} from '../../utils/connect';
 import type { UrlState, UrlSetupPhase } from '../../types/state';
 import type { Profile } from '../../types/profile';
 
@@ -80,15 +86,16 @@ class UrlManager extends React.PureComponent<Props> {
   async _processInitialUrls() {
     const {
       startFetchingProfiles,
-      getProfilesFromRawUrl,
       setupInitialUrlState,
       urlSetupDone,
     } = this.props;
+    // We have to wrap this because of the error introduced by upgrading to v0.96.0. See issue #1936.
+    const getProfilesFromRawUrl: WrapFunctionInDispatch<GetProfilesFromRawUrl> = (this
+      .props.getProfilesFromRawUrl: any);
     startFetchingProfiles();
 
     try {
       // Process the raw url and fetch the profile.
-      // $FlowFixMe Error introduced by upgrading to v0.96.0. See issue #1936.
       const results: {
         profile: Profile,
         shouldSetupInitialUrlState: boolean,
