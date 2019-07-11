d543be93ed5d0975d41529cfb0f5bd5c526fc971
Lucas <lucas.keiler1984@gmail.com>
2019-07-11 17:11:24 -0300

    Change bisect to ES6 module

--- src/utils/bisect.js
+++ src/utils/bisect.js
@@ -1,27 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /*
-Copyright (c) 2010/2011 Arnout Kazemier,3rd-Eden
+    Copyright (c) 2010/2011 Arnout Kazemier,3rd-Eden
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE. 
 */
 
 // @flow
-'use strict';
 
 /**
  * Calculates the index of the Array where item X should be placed, assuming the Array is sorted.
@@ -32,13 +35,13 @@ THE SOFTWARE.
  * @param {Number} high The maximum Index that is used to stop searching, optional.
  * @returns {Number} the index where item X should be placed
  */
-function bisection(array, x, low, high){
+export function bisectionRight(array, x, low, high): Number {
   // The low and high bounds the inital slice of the array that needs to be searched
   // this is optional
   low = low || 0;
   high = high || array.length;
 
-  var mid;
+  let mid;
 
   while (low < high) {
     mid = (low + high) >> 1;
@@ -53,10 +56,6 @@ function bisection(array, x, low, high){
   return low;
 }
 
-/**
- * A right bisection is default, so just reference the same function
- */
-bisection.right = bisection;
 
 /**
  * Calculates the index of the Array where item X should be placed, assuming the Array is sorted.
@@ -66,13 +65,13 @@ bisection.right = bisection;
  * @param {number} high The maximum Index that is used to stop searching, optional.
  * @return {number} the index where item X should be placed
  */
-bisection.left = function left( array, x, low , high ){
+export function bisectionLeft(array, x, low, high): Number {
   // The low and high bounds the inital slice of the array that needs to be searched
   // this is optional
   low = low || 0;
   high = high || array.length;
 
-  var mid;
+  let mid;
 
   while (low < high) {
     mid = (low + high) >> 1;
@@ -85,11 +84,4 @@ bisection.left = function left( array, x, low , high ){
   }
 
   return low;
-};
-
-/**
- * Library version
- */
-bisection.version = '0.0.3';
-
-module.exports = bisection;
+}
