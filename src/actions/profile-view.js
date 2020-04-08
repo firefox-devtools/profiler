e012fa02a3d8c6ee2e025ae6507cff5379f58bed
Nazım Can Altınova <466239+canova@users.noreply.github.com>
2020-04-08 15:54:37 +0200

    Switch if-else statement into a switch-case to make the following changes more readable (PR #2488)

--- src/actions/profile-view.js
+++ src/actions/profile-view.js
@@ -230,73 +230,89 @@ export function selectTrack(trackReference: TrackReference): ThunkAction<void> {
     let selectedThreadIndex = null;
     let selectedTab = currentlySelectedTab;
 
-    if (trackReference.type === 'global') {
-      // Handle the case of global tracks.
-      const globalTrack = getGlobalTrackFromReference(
-        getState(),
-        trackReference
-      );
+    switch (trackReference.type) {
+      case 'global': {
+        // Handle the case of global tracks.
+        const globalTrack = getGlobalTrackFromReference(
+          getState(),
+          trackReference
+        );
 
-      // Go through each type, and determine the selected slug and thread index.
-      switch (globalTrack.type) {
-        case 'process': {
-          if (globalTrack.mainThreadIndex === null) {
-            // Do not allow selecting process tracks without a thread index.
+        // Go through each type, and determine the selected slug and thread index.
+        switch (globalTrack.type) {
+          case 'process': {
+            if (globalTrack.mainThreadIndex === null) {
+              // Do not allow selecting process tracks without a thread index.
+              return;
+            }
+            selectedThreadIndex = globalTrack.mainThreadIndex;
+            // Ensure a relevant thread-based tab is used.
+            if (selectedTab === 'network-chart') {
+              selectedTab = getLastVisibleThreadTabSlug(getState());
+            }
+            break;
+          }
+          case 'screenshots':
+          case 'visual-progress':
+          case 'perceptual-visual-progress':
+          case 'contentful-visual-progress':
+            // Do not allow selecting these tracks.
             return;
-          }
-          selectedThreadIndex = globalTrack.mainThreadIndex;
-          // Ensure a relevant thread-based tab is used.
-          if (selectedTab === 'network-chart') {
-            selectedTab = getLastVisibleThreadTabSlug(getState());
-          }
-          break;
+          default:
+            throw assertExhaustiveCheck(
+              globalTrack,
+              `Unhandled GlobalTrack type.`
+            );
         }
-        case 'screenshots':
-        case 'visual-progress':
-        case 'perceptual-visual-progress':
-        case 'contentful-visual-progress':
-          // Do not allow selecting these tracks.
-          return;
-        default:
-          throw assertExhaustiveCheck(
-            globalTrack,
-            `Unhandled GlobalTrack type.`
-          );
+        break;
       }
-    } else {
-      // Handle the case of local tracks.
-      const localTrack = getLocalTrackFromReference(getState(), trackReference);
+      case 'local': {
+        // Handle the case of local tracks.
+        const localTrack = getLocalTrackFromReference(
+          getState(),
+          trackReference
+        );
 
-      // Go through each type, and determine the tab slug and thread index.
-      switch (localTrack.type) {
-        case 'thread': {
-          // Ensure a relevant thread-based tab is used.
-          selectedThreadIndex = localTrack.threadIndex;
-          if (selectedTab === 'network-chart') {
-            selectedTab = getLastVisibleThreadTabSlug(getState());
+        // Go through each type, and determine the tab slug and thread index.
+        switch (localTrack.type) {
+          case 'thread': {
+            // Ensure a relevant thread-based tab is used.
+            selectedThreadIndex = localTrack.threadIndex;
+            if (selectedTab === 'network-chart') {
+              selectedTab = getLastVisibleThreadTabSlug(getState());
+            }
+            break;
           }
-          break;
+          case 'network':
+            selectedThreadIndex = localTrack.threadIndex;
+            selectedTab = 'network-chart';
+            break;
+          case 'ipc':
+            selectedThreadIndex = localTrack.threadIndex;
+            selectedTab = 'marker-chart';
+            break;
+          case 'memory': {
+            const { counterIndex } = localTrack;
+            const counterSelectors = getCounterSelectors(counterIndex);
+            const counter = counterSelectors.getCommittedRangeFilteredCounter(
+              getState()
+            );
+            selectedThreadIndex = counter.mainThreadIndex;
+            break;
+          }
+          default:
+            throw assertExhaustiveCheck(
+              localTrack,
+              `Unhandled LocalTrack type.`
+            );
         }
-        case 'network':
-          selectedThreadIndex = localTrack.threadIndex;
-          selectedTab = 'network-chart';
-          break;
-        case 'ipc':
-          selectedThreadIndex = localTrack.threadIndex;
-          selectedTab = 'marker-chart';
-          break;
-        case 'memory': {
-          const { counterIndex } = localTrack;
-          const counterSelectors = getCounterSelectors(counterIndex);
-          const counter = counterSelectors.getCommittedRangeFilteredCounter(
-            getState()
-          );
-          selectedThreadIndex = counter.mainThreadIndex;
-          break;
-        }
-        default:
-          throw assertExhaustiveCheck(localTrack, `Unhandled LocalTrack type.`);
+        break;
       }
+      default:
+        throw assertExhaustiveCheck(
+          trackReference,
+          'Unhandled TrackReference type'
+        );
     }
 
     const doesNextTrackHaveSelectedTab = getThreadSelectors(selectedThreadIndex)
