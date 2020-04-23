4bbf8d1cb591afead69467b80799bdc874d46154
Markus Stange <mstange@themasta.com>
2020-04-23 01:56:53 -0400

    Add tests for re-symbolication.

--- src/test/fixtures/example-symbol-table.js
+++ src/test/fixtures/example-symbol-table.js
@@ -6,22 +6,32 @@
 import { TextEncoder } from 'util';
 import type { SymbolTableAsTuple } from '../../profile-logic/symbol-store-db';
 
-const syms = {
+const completeSyms = {
   addresses: [0, 0xf00, 0x1a00, 0x2000],
   symbols: ['first symbol', 'second symbol', 'third symbol', 'last symbol'],
 };
 
-const index = [0];
-let accum = 0;
-for (const sym of syms.symbols) {
-  accum += sym.length;
-  index.push(accum);
+const partialSyms = {
+  addresses: [0, 0x2000],
+  symbols: ['overencompassing first symbol', 'last symbol'],
+};
+
+function _makeSymbolTableAsTuple(syms): SymbolTableAsTuple {
+  const index = [0];
+  let accum = 0;
+  for (const sym of syms.symbols) {
+    accum += sym.length;
+    index.push(accum);
+  }
+
+  return [
+    new Uint32Array(syms.addresses),
+    new Uint32Array(index),
+    new TextEncoder().encode(syms.symbols.join('')),
+  ];
 }
 
-const symbolTable: SymbolTableAsTuple = [
-  new Uint32Array(syms.addresses),
-  new Uint32Array(index),
-  new TextEncoder().encode(syms.symbols.join('')),
-];
+export const completeSymbolTable = _makeSymbolTableAsTuple(completeSyms);
+export const partialSymbolTable = _makeSymbolTableAsTuple(partialSyms);
 
-export default symbolTable;
+export default completeSymbolTable;
