988a6398a4e0529c93499aa321617c0f8aea3618
Greg Tatum <tatum.creative@gmail.com>
2020-05-21 14:24:10 -0500

    Add the profile upgrader for weights

--- src/profile-logic/processed-profile-versioning.js
+++ src/profile-logic/processed-profile-versioning.js
@@ -1188,5 +1188,27 @@ const _upgraders = {
       }
     }
   },
+  [29]: profile => {
+    // The sample and allocation properties "duration" were changed to "weight"
+    for (const thread of profile.threads) {
+      if (thread.samples.duration) {
+        thread.samples.weightType = 'samples';
+        thread.samples.weight = thread.samples.duration;
+        delete thread.samples.duration;
+      }
+
+      if (thread.nativeAllocations) {
+        thread.nativeAllocations.weightType = 'bytes';
+        thread.nativeAllocations.weight = thread.nativeAllocations.duration;
+        delete thread.nativeAllocations.duration;
+      }
+
+      if (thread.jsAllocations) {
+        thread.jsAllocations.weightType = 'bytes';
+        thread.jsAllocations.weight = thread.jsAllocations.duration;
+        delete thread.jsAllocations.duration;
+      }
+    }
+  },
 };
 /* eslint-enable no-useless-computed-key */
