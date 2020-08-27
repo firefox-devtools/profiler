83ff67b6773e42a39b9ec3004aba8af6bcc59705
Julien Wajsberg <felash@gmail.com>
2020-08-27 18:42:44 +0200

    Improve the navigation mocks

--- src/test/fixtures/mocks/window-navigation.js
+++ src/test/fixtures/mocks/window-navigation.js
@@ -3,37 +3,71 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // @flow
-//
-import { coerceMatchingShape } from '../../../utils/flow';
 
 /**
- * jsdom's implementation of this API doesn't allow for assigning to the location, which
- * we need to be able to do for certain tests.
+ * jsdom leaves the history in place after every test, so the history will
+ * be dirty. Also its implementation for window.location is lacking severely.
+ * So this file implements mocks for both window.history and
+ * window.location. They work together so that history methods update the
+ * location, and changing the location pushes a new state in the history.
+ *
+ * There are 2 exports:
+ * - autoMockFullNavigation
+ * - mockFullNavigation
+ *
+ * `autoMockFullNavigation` is the simplest: calling it in a test file (outside
+ * any test) will register beforeEach and afterEach lifecycle functions to take
+ * care of the cleanup automatically.`
+ *
+ * describe('SomeFile', () => {
+ *   autoMockFullNavigation()
+ *
+ *   test('it supports history', () => {
+ *     ...
+ *   });
+ * });
+ *
+ * If you want to start with a specific URL, you can use
+ * `window.location.replace` or `window.history.replaceState` to replace the
+ * default URL.`
+ *
+ * `mockFullNavigation` can be useful if you want more control over the process.
+ * It takes the initial URL as a parameter and returns a cleanup function that
+ * you _must_ call after your test ends.
  */
-export function mockWindowLocation(location: string = 'http://localhost') {
+
+import { coerceMatchingShape } from '../../../utils/flow';
+
+// This symbol will be used in the mock for window.location so that the mock for
+// window.history can change the inner location directly.
+const internalLocationAssign = Symbol.for('internalLocationAssign');
+
+/**
+ * This mock creates a location API that allows for assigning to the location,
+ * which we need to be able to do for certain tests.
+ */
+function mockWindowLocation(location: string = 'http://localhost') {
   // This is the internal state.
   let url = new URL(location);
-  function setLocation(newUrl: string | { toString: () => string }): void {
-    location = newUrl.toString();
-    url = new URL(location);
+
+  function internalSetLocation(
+    newUrl: string | { toString: () => string }
+  ): void {
+    url = new URL(newUrl.toString(), url);
   }
 
   const nativeLocation = Object.getOwnPropertyDescriptor(window, 'location');
-  function accessError() {
-    throw new Error(
-      'Setting properties for the window.location object is not supported with this mock.'
-    );
-  }
 
   // It seems node v8 doesn't let us change the value unless we delete it before.
   delete window.location;
 
   const property = {
-    get(): $Shape<Location> {
+    get() {
       return {
+        toString: () => url.toString(),
         ancestorOrigins: [],
         get href() {
-          return location;
+          return url.toString();
         },
         get origin() {
           return url.origin;
@@ -56,44 +90,62 @@ export function mockWindowLocation(location: string = 'http://localhost') {
         get search() {
           return url.search;
         },
+        get searchParams() {
+          return url.searchParams;
+        },
         get hash() {
           return url.hash;
         },
-        set href(v) {
-          accessError();
-        },
-        set origin(v) {
-          accessError();
+        set href(newUrl) {
+          this.assign(newUrl.toString());
         },
         set protocol(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.protocol = v;
+          this.assign(newUrl.toString());
         },
         set host(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.host = v;
+          this.assign(newUrl.toString());
         },
         set hostname(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.hostname = v;
+          this.assign(newUrl.toString());
         },
         set port(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.port = v;
+          this.assign(newUrl.toString());
         },
         set pathname(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.pathname = v;
+          this.assign(newUrl.toString());
         },
         set search(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.search = v;
+          this.assign(newUrl.toString());
         },
         set hash(v) {
-          accessError();
+          const newUrl = new URL(url.toString());
+          newUrl.hash = v;
+          this.assign(newUrl.toString());
         },
-        assign: setLocation,
+        // $FlowExpectError Flow doesn't know about symbol properties sadly.
+        [internalLocationAssign]: internalSetLocation,
+        assign: (newUrl: string) => window.history.pushState(null, '', newUrl),
         reload: jest.fn(),
-        replace: jest.fn(setLocation),
-        toString: () => location,
+        replace: (newUrl: string) =>
+          window.history.replaceState(null, '', newUrl),
       };
     },
     configurable: true,
-    set: setLocation,
+    set(newUrl: string) {
+      window.history.pushState(null, '', newUrl);
+    },
   };
 
   // $FlowExpectError because the value we pass isn't a proper Location object.
@@ -101,19 +153,22 @@ export function mockWindowLocation(location: string = 'http://localhost') {
 
   // Return a function that resets the mock.
   return () => {
+    // This "delete" call doesn't seem to be necessary, but better do it so that
+    // we don't have surprises in the future.
+    delete window.location;
     // $FlowExpectError because nativeLocation doesn't match the type expected by Flow.
     Object.defineProperty(window, 'location', nativeLocation);
   };
 }
 
 /**
- * jsdom leaves the history in place after every test, so the history will be dirty.
  * This mock creates a history API that can be thrown away after every use.
  */
-export function mockWindowHistory() {
+function mockWindowHistory() {
   const originalHistory = Object.getOwnPropertyDescriptor(window, 'history');
 
   let states = [null];
+  let urls = [window.location.href];
   let index = 0;
   const history = {
     get length() {
@@ -127,6 +182,8 @@ export function mockWindowHistory() {
       }
       index--;
       history.state = states[index];
+      // $FlowExpectError Flow doesn't know about this internal property.
+      window.location[internalLocationAssign](urls[index]);
       window.dispatchEvent(new Event('popstate'));
     },
     forward() {
@@ -136,23 +193,44 @@ export function mockWindowHistory() {
       index++;
 
       history.state = states[index];
+      // $FlowExpectError Flow doesn't know about this internal property.
+      window.location[internalLocationAssign](urls[index]);
       window.dispatchEvent(new Event('popstate'));
     },
     go() {
       throw new Error('Not implemented.');
     },
-    pushState(newState: any, _title: string, _url?: string) {
-      // The title and URL are ignored.
+    pushState(newState: any, _title: string, url?: string) {
+      if (url) {
+        // Let's assign the URL to the window.location mock. This should also
+        // make the URL correct if it's relative, we'll get an absolute URL when
+        // retrieving later through window.location.href.
+        // $FlowExpectError Flow doesn't know about this internal property.
+        window.location[internalLocationAssign](url);
+      }
+
+      urls = urls.slice(0, index + 1);
+      urls.push(window.location.href);
+
       states = states.slice(0, index + 1);
       states.push(newState);
       index++;
     },
-    replaceState(newState: any, _title: string, _url?: string) {
-      // The title and URL are ignored.
+    replaceState(newState: any, _title: string, url?: string) {
+      if (url) {
+        // Let's assign the URL to the window.location mock.
+        // $FlowExpectError Flow doesn't know about this internal property.
+        window.location[internalLocationAssign](url);
+        urls[index] = window.location.href;
+      }
+
       states[index] = newState;
     },
   };
 
+  // This "delete" call doesn't seem to be necessary, but better do it so that
+  // we don't have surprises in the future.
+  delete window.history;
   Object.defineProperty(window, 'history', {
     value: coerceMatchingShape<History>(history),
     configurable: true,
@@ -160,7 +238,42 @@ export function mockWindowHistory() {
 
   // Return a function that resets the mock.
   return () => {
+    // For unknown reasons, we can't assign back the old descriptor without
+    // deleting the current one first... Not deleting would keep the mock
+    // without throwing any error.
+    delete window.history;
     // $FlowExpectError - Flow can't handle getOwnPropertyDescriptor being used on defineProperty.
     Object.defineProperty(window, 'history', originalHistory);
   };
+}
+
+// This mocks both window.location and window.history. See the top of the file
+// for more information.
+export function mockFullNavigation({
+  initialUrl,
+}: $Shape<{ initialUrl: string }> = {}): () => void {
+  const restoreLocation = mockWindowLocation(initialUrl);
+  const restoreHistory = mockWindowHistory();
+
+  return () => {
+    restoreLocation();
+    restoreHistory();
+  };
+}
+
+// This registers lifecycle functions to mock both window.location and
+// window.history for each test. Take a look at the top of this file for more
+// information about how to use this.
+export function autoMockFullNavigation() {
+  let cleanup;
+  beforeEach(() => {
+    cleanup = mockFullNavigation();
+  });
+
+  afterEach(() => {
+    if (cleanup) {
+      cleanup();
+      cleanup = null;
+    }
+  });
 }
