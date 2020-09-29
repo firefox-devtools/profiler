0052b879e4fcd6c0cc22f34eb2a7c857a7c27440
Julien Wajsberg <felash@gmail.com>
2020-09-29 19:00:09 +0200

    Review comments: do not display the title when the dialog is open

--- src/components/shared/ButtonWithPanel/index.js
+++ src/components/shared/ButtonWithPanel/index.js
@@ -139,7 +139,7 @@ export class ButtonWithPanel extends React.PureComponent<Props, State> {
           type="button"
           className={classNames('buttonWithPanelButton', buttonClassName)}
           value={label}
-          title={title}
+          title={open ? null : title}
           onClick={this._onButtonClick}
           ref={buttonRef}
         />
