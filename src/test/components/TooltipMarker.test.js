0700b94ce1eb1a9be9cc08f6b31cf810f64bdb5d
Greg Tatum <tatum.creative@gmail.com>
2020-10-07 09:23:22 -0500

    Remove all references to the Bailout and Invalidation type

--- src/test/components/TooltipMarker.test.js
+++ src/test/components/TooltipMarker.test.js
@@ -243,11 +243,37 @@ describe('TooltipMarker', function() {
           },
         },
       ],
+      // This is an old-style bailout marker. This was changed to a Text marker without
+      // a version bump between Gecko profile version 20-21.
       [
         'Bailout_ShapeGuard after getelem on line 3666 of resource://foo.js -> resource://bar.js:3662',
         10,
       ],
+      // This is an old-style invalidation marker. This was changed to a Text marker without
+      // a version bump between Gecko profile version 20-21.
       ['Invalidate http://mozilla.com/script.js:1234', 10],
+      // This is a bailout text marker, as of Gecko profile version 20-21
+      [
+        'Bailout',
+        10,
+        null,
+        {
+          type: 'Text',
+          name:
+            'NonObjectInput at JumpTarget on line 27 of https://profiler.firefox.com/701f018d7923ccd65ba7.bundle.js:27',
+        },
+      ],
+      // This is a Invalidate text marker, as of Gecko profile version 20-21
+      [
+        'Invalidate',
+        10,
+        null,
+        {
+          type: 'Text',
+          name:
+            'https://profiler.firefox.com/701f018d7923ccd65ba7.bundle.js:198:23518',
+        },
+      ],
       [
         'Styles',
         18.5,
