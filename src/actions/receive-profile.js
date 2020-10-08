ea4440a9391e23d3e8ee1093509c9d838ac95706
codershona <falgunimst95@gmail.com>
2020-10-08 18:02:28 +0200

    migratingrelative paths into actions, receive-profile.js file

--- src/actions/receive-profile.js
+++ src/actions/receive-profile.js
@@ -8,17 +8,14 @@ import queryString from 'query-string';
 import {
   processProfile,
   unserializeProfileOfArbitraryFormat,
-} from '../profile-logic/process-profile';
-import { SymbolStore } from '../profile-logic/symbol-store';
-import {
-  symbolicateProfile,
-  applySymbolicationStep,
-} from '../profile-logic/symbolication';
-import * as MozillaSymbolicationAPI from '../profile-logic/mozilla-symbolication-api';
-import { mergeProfilesForDiffing } from '../profile-logic/merge-compare';
-import { decompress } from '../utils/gz';
-import { expandUrl } from '../utils/shorten-url';
-import { TemporaryError } from '../utils/errors';
+} from 'firefox-profiler/profile-logic/process-profile';
+import { SymbolStore } from 'firefox-profiler/profile-logic/symbol-store';
+import { symbolicateProfile, applySymbolicationStep } from 'firefox-profiler/profile-logic/symbolication';
+import * as MozillaSymbolicationAPI from 'firefox-profiler/profile-logic/mozilla-symbolication-api';
+import { mergeProfilesForDiffing } from 'firefox-profiler/profile-logic/merge-compare';
+import { decompress } from 'firefox-profiler/utils/gz';
+import { expandUrl } from 'firefox-profiler/utils/shorten-url';
+import { TemporaryError } from 'firefox-profiler/utils/errors';
 import JSZip from 'jszip';
 import {
   getSelectedThreadIndexesOrNull,
@@ -37,7 +34,7 @@ import {
 import {
   stateFromLocation,
   getDataSourceFromPathParts,
-} from '../app-logic/url-handling';
+} from 'firefox-profiler/app-logic/url-handling';
 import {
   initializeLocalTrackOrderByPid,
   initializeHiddenLocalTracksByPid,
@@ -47,10 +44,10 @@ import {
   initializeSelectedThreadIndex,
   initializeHiddenGlobalTracks,
   getVisibleThreads,
-} from '../profile-logic/tracks';
-import { computeActiveTabTracks } from '../profile-logic/active-tab';
-import { setDataSource } from './profile-view';
-import { fatalError } from './errors';
+} from 'firefox-profiler/profile-logic/tracks';
+import { computeActiveTabTracks } from 'firefox-profiler/profile-logic/active-tab';
+import { setDataSource } from 'firefox-profiler/actions/profile-view';
+import { fatalError } from 'firefox-profiler/actions/errors';
 import { GOOGLE_STORAGE_BUCKET } from 'firefox-profiler/app-logic/constants';
 
 import type {
@@ -70,8 +67,8 @@ import type {
   OriginsTimelineRoot,
 } from 'firefox-profiler/types';
 
-import type { SymbolicationStepInfo } from '../profile-logic/symbolication';
-import { assertExhaustiveCheck, ensureExists } from '../utils/flow';
+import type { SymbolicationStepInfo } from 'firefox-profiler/profile-logic/symbolication';
+import { assertExhaustiveCheck, ensureExists } from 'firefox-profiler/utils/flow';
 
 /**
  * This file collects all the actions that are used for receiving the profile in the
