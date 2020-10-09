448235d01e68a482d131b22a7861b282020e09bb
codershona <falgunimst95@gmail.com>
2020-10-09 20:44:43 +0200

    linting: 14

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import '../res/css/network.css';
 import 'react-splitter-layout/lib/index.css';
 import React from 'react';
 import { render } from 'react-dom';
-import Root from 'firefox-profiler/components/app/Root';
+import { Root } from 'firefox-profiler/components/app/Root';
 import createStore from 'firefox-profiler/app-logic/create-store';
 import {
   addDataToWindowObject,
