7b81e52e56591a0881a0ce76d82d6982cebf1a3c
km-js <kopal.mehta22@gmail.com>
2020-10-10 20:08:18 +0530

    Displayed timestamps to the stacks

--- src/components/tooltip/Marker.js
+++ src/components/tooltip/Marker.js
@@ -312,12 +312,15 @@ class MarkerTooltipContents extends React.PureComponent<Props> {
         <TooltipDetailSeparator key="backtrace-separator" />,
         <TooltipDetail label="Stack" key="backtrace">
           <div className="tooltipDetailsBackTrace">
+          <h2 className="tooltipBackTraceTitle">
             {data.type === 'Styles' || marker.name === 'Reflow' ? (
-              <h2 className="tooltipBackTraceTitle">
-                First invalidated {formatNumber(causeAge)}ms before the flush,
-                at:
-              </h2>
-            ) : null}
+                `First invalidated ${formatNumber(causeAge)}ms before the flush,
+                at:`
+            ) : (
+              `Triggered ${formatNumber(causeAge)}ms ago,
+              at:`
+            )}
+            </h2>
             <Backtrace
               maxStacks={restrictHeightWidth ? 20 : Infinity}
               stackIndex={cause.stack}
