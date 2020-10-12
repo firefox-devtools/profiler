80542477e40c0a3f75beec6647835cb1bca2a952
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-12 11:00:20 +0100

    Add default width of 600px to style object in tooltip.js
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- src/components/tooltip/Tooltip.js
+++ src/components/tooltip/Tooltip.js
@@ -129,6 +129,7 @@ export class Tooltip extends React.PureComponent<Props, State> {
     const style = {
       left,
       top,
+      --tooltip-detail-max-width: '600px'
     };
 
     return ReactDOM.createPortal(
