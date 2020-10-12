865193bd36f8f48a01561b60d83618ea8c083ea3
Nazım Can Altınova <canaltinova@gmail.com>
2020-10-12 12:33:50 +0200

    Put back the empty line in the test file

--- src/test/components/Details.test.js
+++ src/test/components/Details.test.js
@@ -14,6 +14,7 @@ import { getProfileFromTextSamples } from '../fixtures/profiles/processed-profil
 
 import { tabSlugs } from '../../app-logic/tabs-handling';
 import type { TabSlug } from '../../app-logic/tabs-handling';
+
 // Let's mock all possible views so that we don't spend too much time rendering stuff.
 // We use the tab slugs as class names. `call-tree` is an exception because if
 // we need a dash to masquerade as custom elements so that React doesn't emit a
