9f980e230b2341ce0ab19998f5ecc3d138a8b148
Riju19 <19.riju@gmail.com>
2020-10-17 20:55:04 +0530

    Make cosmetic changes to bisect.js
    Export two functions using ES6 modules: bisectionLeft and bisectionRight. Add comments about the original source of this code and the description of the two functions. Add flow typing.

--- src/utils/bisect.js
+++ src/utils/bisect.js
@@ -1,24 +1,42 @@
-'use strict';
+// This file originally comes from https://github.com/3rd-Eden/node-bisection/
 
-/**
- * Calculates the index of the Array where item X should be placed, assuming the Array is sorted.
- *
- * @param {Array} array The array containing the items.
- * @param {Number} x The item that needs to be added to the array.
- * @param {Number} low Inital Index that is used to start searching, optional.
- * @param {Number} high The maximum Index that is used to stop searching, optional.
- * @returns {Number} the index where item X should be placed
- */
-function bisection(array, x, low, high) {
-  // The low and high bounds the inital slice of the array that needs to be searched
-  // this is optional
+/*
+    Copyright (c) 2010/2011 Arnout Kazemier,3rd-Eden
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE. 
+*/
+
+/*
+    The two functions here, bisectionLeft and bisectionRight, return the index where a new element
+    would be inserted, respectively at the left or at the right of elements with the same value. 
+*/
+
+// @flow
+
+export function bisectionRight(
+  array: number[] | $TypedArray,
+  x: number,
+  low?: number,
+  high?: number
+): number {
   low = low || 0;
   high = high || array.length;
 
-  var mid;
-
   while (low < high) {
-    mid = (low + high) >> 1;
+    const mid = (low + high) >> 1;
 
     if (x < array[mid]) {
       high = mid;
@@ -30,29 +48,17 @@ function bisection(array, x, low, high) {
   return low;
 }
 
-/**
- * A right bisection is default, so just reference the same function
- */
-bisection.right = bisection;
-
-/**
- * Calculates the index of the Array where item X should be placed, assuming the Array is sorted.
- * @param {Array} array The array containing the items.
- * @param {number} x The item that needs to be added to the array.
- * @param {number} low Inital Index that is used to start searching, optional.
- * @param {number} high The maximum Index that is used to stop searching, optional.
- * @return {number} the index where item X should be placed
- */
-bisection.left = function left(array, x, low, high) {
-  // The low and high bounds the inital slice of the array that needs to be searched
-  // this is optional
+export function bisectionLeft(
+  array: number[] | $TypedArray,
+  x: number,
+  low?: number,
+  high?: number
+): number {
   low = low || 0;
   high = high || array.length;
 
-  var mid;
-
   while (low < high) {
-    mid = (low + high) >> 1;
+    const mid = (low + high) >> 1;
 
     if (x < array[mid]) {
       low = mid + 1;
@@ -62,11 +68,4 @@ bisection.left = function left(array, x, low, high) {
   }
 
   return low;
-};
-
-/**
- * Library version
- */
-bisection.version = '0.0.3';
-
-module.exports = bisection;
+}
