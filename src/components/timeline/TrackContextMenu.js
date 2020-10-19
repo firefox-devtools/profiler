111292f82cf2558b942b3e6e6502d64cec3616d5
codershona <falgunimst95@gmail.com>
2020-10-19 19:35:49 +0200

    creating a function using getrightclicktracktype

--- src/components/timeline/TrackContextMenu.js
+++ src/components/timeline/TrackContextMenu.js
@@ -22,7 +22,7 @@ import {
   getThreads,
   getRightClickedTrack,
   getGlobalTracks,
-  getRightClickedThreadIndex,
+  getRightClickedTrackType
   getLocalTrackNamesByPid,
   getGlobalTrackNames,
   getLocalTracksByPid,
@@ -295,6 +295,21 @@ class TimelineTrackContextMenu extends PureComponent<Props> {
     return localTrackNames[rightClickedTrack.trackIndex];
   }
 
+  getRightClickedTrackType(rightClickedTrack: TrackReference): string{
+     const { globalTrackNames, localTrackNamesByPid } = this.props;
+
+     if (rightClickedTrack.type === 'global') {
+      return globalTrackNames[rightClickedTrack.trackIndex + 'hide-all'];
+    }
+    const localTrackNames = localTrackNamesByPid.get(rightClickedTrack.pid);
+    if (localTrackNames === 'local') {
+    return localTrackNames[rightClickedTrack.trackIndex + 'hide-all'];
+
+    }
+    
+   }    
+  }
+
   renderIsolateProcess() {
     const {
       rightClickedTrack,
@@ -530,7 +545,7 @@ class TimelineTrackContextMenu extends PureComponent<Props> {
           data={(rightClickedTrack, globalTracks, localTracksByPid)}
           onClick={this._toggleGlobalTrackVisibility}
         >
-          Hide all {`"${this.getRightClickedTrackName(rightClickedTrack)}"`}{' '}
+          Hide all {`"${this.getRightClickedTrackType(rightClickedTrack)}"`}{' '}
           tracks
         </MenuItem>
       );
