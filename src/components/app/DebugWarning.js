754211d8f1080295c5134292fc1db824c3bb0453
Nazım Can Altınova <canaltinova@gmail.com>
2020-10-21 14:37:10 +0200

    Some minor cosmetic changes related to spacing

--- src/components/app/DebugWarning.js
+++ src/components/app/DebugWarning.js
@@ -1,11 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // @flow
+
 import React, { PureComponent } from 'react';
 import { Warning } from '../shared/Warning';
 import explicitConnect from '../../utils/connect';
 import { getMeta } from '../../selectors/profile';
+
 import type { ProfileMeta } from 'firefox-profiler/types';
 import type { ConnectedProps } from '../../utils/connect';
 
