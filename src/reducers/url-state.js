c78f19292d924420ea3bca6da8e0543e4d75a2fe
Cheederah <misscheederah@gmail.com>
2020-10-21 06:35:19 +0100

    Fixing popState and UrlState management for dragging/dropping files and updating snapshots

--- src/reducers/url-state.js
+++ src/reducers/url-state.js
@@ -511,7 +511,7 @@ const wrapReducerInResetter = (
         // A new URL came in because of a browser action, discard the current UrlState
         // and use the new one, which was probably serialized from the URL, or stored
         // in the history API.
-        return action.newUrlState
+        return action.newUrlState.profileUrl !== ''
           ? action.newUrlState
           : regularUrlStateReducer(undefined, action);
       case 'RETURN_TO_ZIP_FILE_LIST':
