c8fab795d0f328798f71f5312d9a577c8ab441c7
codershona <falgunimst95@gmail.com>
2020-10-21 17:25:50 +0200

    correction: 7

--- src/components/flame-graph/Canvas.js
+++ src/components/flame-graph/Canvas.js
@@ -405,7 +405,7 @@ class FlameGraphCanvasImpl extends React.PureComponent<Props> {
     }
     const {
       maxStackDepth,
-      categories,
+      flameGraphTiming,
       thread,
       weightType,
     } = this.props;
@@ -417,7 +417,7 @@ class FlameGraphCanvasImpl extends React.PureComponent<Props> {
       depthItem
     );
 
-    categories({
+    flameGraphTiming({
       hasSelection: true,
       isModifying: true,
       selectionStart: start,
