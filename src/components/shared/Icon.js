400be4b3ac93d69ac54e281b1ae9e2abf6bd3516
Abir <abir.pal899@gmail.com>
2020-10-22 20:00:21 +0530

    In Icon.js, moved the code of componentWillReceiveProps to componentDidUpdate (PR #2937)
    Part of #1006

--- src/components/shared/Icon.js
+++ src/components/shared/Icon.js
@@ -41,9 +41,9 @@ class IconImpl extends PureComponent<Props> {
     }
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.icon) {
-      nextProps.iconStartLoading(nextProps.icon);
+  componentDidUpdate() {
+    if (this.props.icon) {
+      this.props.iconStartLoading(this.props.icon);
     }
   }
 
