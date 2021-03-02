7c1298da690d96ae6827cd16c1c73077e90388c7
Hasna Hena Mow <37832457+CipherGirl@users.noreply.github.com>
2021-03-03 00:59:26 +0600

    Localize the Home Page (PR #3123)

--- locales/en-US/app.ftl
+++ locales/en-US/app.ftl
@@ -1,8 +1,74 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 ### Localization for the App UI of Profiler
 
-# Naming convention for l10n IDs: "module-ComponentName--string-summary".
+# Naming convention for l10n IDs: "ComponentName--string-summary".
 # This allows us to minimize the risk of conflicting IDs throughout the app.
-# Please sort alphabetically by (module name, component name). For each module,
+# Please sort alphabetically by (component name), and
 # keep strings in order of appearance.
 
-intro = Web app for Firefox performance analysis
+## The following feature names must be treated as a brand. They cannot be translated.
+
+-firefox-brand-name = Firefox
+-profiler-brand-name = Firefox Profiler
+
+##
+
+AppHeader--app-header = <header>{ -profiler-brand-name }</header> — <subheader>Web app for { -firefox-brand-name } performance analysis</subheader>
+AppHeader--github-icon =
+    .title = Go to our git repository (this opens in a new window)
+
+FooterLinks--legal = Legal
+FooterLinks--Privacy = Privacy
+FooterLinks--Cookies = Cookies
+
+Home--upload-from-file-input-button = Load a profile from file
+Home--upload-from-url-button = Load a profile from a URL
+Home--load-from-url-submit-button =
+    .value = Load
+
+Home--documentation-button = Documentation
+Home--record-instructions =
+    To start profiling, click on the profiling button, or use the
+    keyboard shortcuts. The icon is blue when a profile is recording. 
+    Hit <kbd>Capture Profile</kbd> to load the data into profiler.firefox.com.
+
+Home--instructions-title = How to view and record profiles
+Home--instructions-content =
+    Recording performance profiles requires <a>{ -firefox-brand-name }</a>.
+    However, existing profiles can be viewed in any modern browser.
+
+Home--record-instructions-start-stop = Stop and start profiling
+Home--record-instructions-capture-load = Capture and load profile
+Home--profiler-motto = Capture a performance profile. Analyze it. Share it. Make the web faster.
+Home--additional-content-title = Load existing profiles
+Home--additional-content-content = You can <strong>drag and drop</strong> a profile file here to load it, or:
+Home--compare-recordings-info = You can also compare recordings. <a>Open the comparing interface.</a>
+Home--recent-uploaded-recordings-title = Recent uploaded recordings
+
+# This string is used on the tooltip of the published profile links.
+# Variables:
+#   $smallProfileName (String) - Shortened name for the published Profile.
+ListOfPublishedProfiles--publishedProfilesLink =
+    .title = Click here to load profile { $smallProfileName }
+
+ListOfPublishedProfiles--uploaded-profile-information-list-empty = No profile has been uploaded yet!
+
+# This string is used below the 'Recent uploaded recordings' list section.
+# Variables:
+#   $profilesRestCount (Number) - Remaining numbers of the uploaded profiles which are not listed under 'Recent uploaded recordings'.
+ListOfPublishedProfiles--uploaded-profile-information-label = See and manage all your recordings ({ $profilesRestCount } more)
+
+# Depending on the number of uploaded profiles, the message is different.
+# Variables:
+#   $uploadedProfileCount (Number) - Total numbers of the uploaded profiles.
+ListOfPublishedProfiles--uploaded-profile-information-list =
+    { $uploadedProfileCount ->
+        [one] Manage this recording
+       *[other] Manage these recordings
+    }
+
+ProfileRootMessage--title = { -profiler-brand-name }
+ProfileRootMessage--additional = Back to home
--- src/components/app/AppHeader.js
+++ src/components/app/AppHeader.js
@@ -13,42 +13,59 @@ import * as React from 'react';
 import { InnerNavigationLink } from 'firefox-profiler/components/shared/InnerNavigationLink';
 
 import './AppHeader.css';
+import { Localized } from '@fluent/react';
 
 export class AppHeader extends React.PureComponent<{||}> {
   render() {
     return (
       <header>
         <h1 className="appHeader">
-          <span className="appHeaderSlogan">
-            <InnerNavigationLink dataSource="none" className="appHeaderLink">
-              Firefox Profiler
-            </InnerNavigationLink>
-            <span className="appHeaderSubtext">
-              {' '}
-              &mdash; Web app for Firefox performance analysis
-            </span>
-          </span>
-          <a
-            className="appHeaderGithubIcon"
-            href="https://github.com/firefox-devtools/profiler"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Go to our git repository (this opens in a new window)"
+          <Localized
+            id="AppHeader--app-header"
+            elems={{
+              header: (
+                // $FlowExpectError Flow doesn't know about this fluent rule for react component.
+                <InnerNavigationLink
+                  dataSource="none"
+                  className="appHeaderLink"
+                ></InnerNavigationLink>
+              ),
+              subheader: <span className="appHeaderSubtext"></span>,
+            }}
           >
-            <svg
-              width="22"
-              height="22"
-              className="octicon octicon-mark-github"
-              viewBox="0 0 16 16"
-              version="1.1"
-              aria-label="github"
+            <span className="appHeaderSlogan">
+              <InnerNavigationLink dataSource="none" className="appHeaderLink">
+                Firefox Profiler
+              </InnerNavigationLink>{' '}
+              –{' '}
+              <span className="appHeaderSubtext">
+                Web app for Firefox performance analysis
+              </span>
+            </span>
+          </Localized>
+          <Localized id="AppHeader--github-icon" attrs={{ title: true }}>
+            <a
+              className="appHeaderGithubIcon"
+              href="https://github.com/firefox-devtools/profiler"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Go to our git repository (this opens in a new window)"
             >
-              <path
-                fillRule="evenodd"
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-              />
-            </svg>
-          </a>
+              <svg
+                width="22"
+                height="22"
+                className="octicon octicon-mark-github"
+                viewBox="0 0 16 16"
+                version="1.1"
+                aria-label="github"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                />
+              </svg>
+            </a>
+          </Localized>
         </h1>
       </header>
     );
