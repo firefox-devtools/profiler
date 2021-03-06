e7765ca2831b061e04c848a91a5aa4454e842d56
Markus Stange <markus.stange@gmail.com>
2021-03-05 22:58:16 -0500

    Add a unit test for inferJavaCategory.

--- src/test/unit/art-trace.test.js
+++ src/test/unit/art-trace.test.js
@@ -2,9 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
-import { getSpecialCategory } from '../../profile-logic/import/art-trace';
+import {
+  getSpecialCategory,
+  CategoryInfo,
+} from '../../profile-logic/import/art-trace';
 
-describe('special-category', function() {
+describe('category-info', function() {
   const exampleMethodsForKnownCategories = [
     {
       methodId: 1,
@@ -137,5 +140,77 @@ describe('special-category', function() {
       'com.arttracetest.',
       'org.arttracetest.',
     ]);
+  });
+
+  it('should infer the right categories for these example methods', function() {
+    const categoryInfo = new CategoryInfo([
+      // One "mozilla" method, two "arttracetest" methods.
+      {
+        methodId: 1,
+        className: 'org.mozilla.Factory',
+        methodName: 'createObject',
+        signature: '<unused>',
+      },
+      {
+        methodId: 1,
+        className: 'com.arttracetest.ArtTraceTester',
+        methodName: 'inTheMajorityCom',
+        signature: '<unused>',
+      },
+      {
+        methodId: 1,
+        className: 'org.arttracetest.ArtTraceTester',
+        methodName: 'inTheMajorityOrg',
+        signature: '<unused>',
+      },
+      ...exampleMethodsForKnownCategories,
+    ]);
+
+    expect(categoryInfo.categories[categoryInfo.specialCategory].name).toEqual(
+      'Arttracetest'
+    );
+
+    expect(
+      categoryInfo.inferJavaCategory(
+        'com.android.internal.policy.PhoneWindow.installDecor'
+      )
+    ).toBe(categoryInfo.androidCategory);
+    expect(
+      categoryInfo.inferJavaCategory(
+        'java.lang.reflect.Proxy.getProxyClass0.getProxyClass0'
+      )
+    ).toBe(categoryInfo.javaCategory);
+    expect(
+      categoryInfo.inferJavaCategory('sun.misc.Unsafe.compareAndSwapObject')
+    ).toBe(categoryInfo.javaCategory);
+    expect(
+      categoryInfo.inferJavaCategory(
+        'kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith'
+      )
+    ).toBe(categoryInfo.kotlinCategory);
+    expect(
+      categoryInfo.inferJavaCategory('kotlinx.coroutines.DispatchedTask.run')
+    ).toBe(categoryInfo.kotlinCategory);
+    expect(
+      categoryInfo.inferJavaCategory('androidx.collection.SimpleArrayMap.put')
+    ).toBe(categoryInfo.androidxCategory);
+    expect(
+      categoryInfo.inferJavaCategory(
+        'androidx.constraintlayout.solver.widgets.ConstraintWidgetContainer.measure'
+      )
+    ).toBe(categoryInfo.androidxCategory);
+    expect(
+      categoryInfo.inferJavaCategory('org.arttracetest.ArtTraceVerifier.verify')
+    ).toBe(categoryInfo.specialCategory);
+    expect(
+      categoryInfo.inferJavaCategory(
+        'com.arttracetest.ArtTraceVerifier2.verify2'
+      )
+    ).toBe(categoryInfo.specialCategory);
+    expect(
+      categoryInfo.inferJavaCategory(
+        'com.mozilla.NotSpecialEnough.noCategoryForYou'
+      )
+    ).toBe(categoryInfo.otherCategory);
   });
 });
