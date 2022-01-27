04d55a0a58d85a1392f28f40406a8209342042ed
Nazım Can Altınova <canaltinova@gmail.com>
2022-01-27 21:56:20 +0100

    Add intersection observer to the memory graph

--- src/components/timeline/TrackMemoryGraph.js
+++ src/components/timeline/TrackMemoryGraph.js
@@ -5,6 +5,7 @@
 // @flow
 
 import * as React from 'react';
+import { InView } from 'react-intersection-observer';
 import { withSize } from 'firefox-profiler/components/shared/WithSize';
 import explicitConnect from 'firefox-profiler/utils/connect';
 import { formatBytes } from 'firefox-profiler/utils/format-numbers';
@@ -57,6 +58,10 @@ type CanvasProps = {|
 class TrackMemoryCanvas extends React.PureComponent<CanvasProps> {
   _canvas: null | HTMLCanvasElement = null;
   _requestedAnimationFrame: boolean = false;
+  _canvasState: {| renderScheduled: boolean, inView: boolean |} = {
+    renderScheduled: false,
+    inView: false,
+  };
 
   drawCanvas(canvas: HTMLCanvasElement): void {
     const {
@@ -177,6 +182,16 @@ class TrackMemoryCanvas extends React.PureComponent<CanvasProps> {
   }
 
   _scheduleDraw() {
+    if (!this._canvasState.inView) {
+      // Canvas is not in the view. Schedule the render for a later intersection
+      // observer callback.
+      this._canvasState.renderScheduled = true;
+      return;
+    }
+
+    // Canvas is in the view. Render the canvas and reset the schedule state.
+    this._canvasState.renderScheduled = false;
+
     if (!this._requestedAnimationFrame) {
       this._requestedAnimationFrame = true;
       window.requestAnimationFrame(() => {
@@ -193,6 +208,16 @@ class TrackMemoryCanvas extends React.PureComponent<CanvasProps> {
     this._canvas = canvas;
   };
 
+  _observerCallback = (inView: boolean, _entry: IntersectionObserverEntry) => {
+    this._canvasState.inView = inView;
+    if (!this._canvasState.renderScheduled) {
+      // Skip if render is not scheduled.
+      return;
+    }
+
+    this._scheduleDraw();
+  };
+
   componentDidMount() {
     this._scheduleDraw();
   }
@@ -203,7 +228,12 @@ class TrackMemoryCanvas extends React.PureComponent<CanvasProps> {
 
   render() {
     return (
-      <canvas className="timelineTrackMemoryCanvas" ref={this._takeCanvasRef} />
+      <InView onChange={this._observerCallback}>
+        <canvas
+          className="timelineTrackMemoryCanvas"
+          ref={this._takeCanvasRef}
+        />
+      </InView>
     );
   }
 }
