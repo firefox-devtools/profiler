7706a9abb27929a9cb0405471c4f9d340c83c698
Julien Wajsberg <felash@gmail.com>
2022-04-26 17:15:25 +0200

    Enable the contents feature in the sidebar

--- docs-user/js/init.js
+++ docs-user/js/init.js
@@ -4,6 +4,7 @@ window.$docsify = {
   auto2top: true,
   loadSidebar: true,
   loadNavbar: true,
+  subMaxLevel: 2,
   // If changing the Google Analytics ID, change it in res/before-load.js as well.
   ga: 'UA-35433268-81',
   // Photon blue-50
