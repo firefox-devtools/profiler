8cb55f80f0532498df534a84077a7de995fdd512
Julien Wajsberg <felash@gmail.com>
2022-04-29 20:23:56 +0200

    Update the sourceview code

--- src/components/shared/SourceView-codemirror.js
+++ src/components/shared/SourceView-codemirror.js
@@ -20,21 +20,23 @@
  *    a cm-nonZeroLine class to them. This highlight line goes across the entire
  *    width of the editor, it covers both the gutter and the main area.
  */
-import { EditorView, Decoration } from '@codemirror/view';
+import {
+  EditorView,
+  Decoration,
+  lineNumbers,
+  GutterMarker,
+  gutter,
+  gutterLineClass,
+} from '@codemirror/view';
 import {
   EditorState,
   StateField,
   StateEffect,
   Compartment,
+  RangeSet,
 } from '@codemirror/state';
-import {
-  lineNumbers,
-  GutterMarker,
-  gutter,
-  gutterLineClass,
-} from '@codemirror/gutter';
-import { RangeSet } from '@codemirror/rangeset';
-import { classHighlightStyle } from '@codemirror/highlight';
+import { syntaxHighlighting } from '@codemirror/language';
+import { classHighlighter } from '@lezer/highlight';
 import { cpp } from '@codemirror/lang-cpp';
 import { rust } from '@codemirror/lang-rust';
 import { javascript } from '@codemirror/lang-javascript';
@@ -239,7 +241,7 @@ export class SourceViewEditor {
         timingsExtension,
         lineNumbers(),
         languageConf.of(_languageExtForPath(path)),
-        classHighlightStyle,
+        syntaxHighlighting(classHighlighter),
         EditorState.readOnly.of(true),
         EditorView.editable.of(false),
       ],
