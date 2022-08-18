c5eca073d380178cc096e9432ecccf92e80ff0f6
Johannes Bechberger <johannes.bechberger@sap.com>
2022-08-18 14:07:53 +0200

    Remove leftover newline

--- src/components/sidebar/CallTreeSidebar.js
+++ src/components/sidebar/CallTreeSidebar.js
@@ -441,7 +441,6 @@ class CallTreeSidebarImpl extends React.PureComponent<Props> {
             <React.Fragment>
               <h4 className="sidebar-title3 sidebar-title-label">
                 <div>Implementation</div>
-
                 <div>Running {getWeightTypeLabel(weightType)}</div>
               </h4>
               <ImplementationBreakdown
