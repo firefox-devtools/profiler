1cd2017b067c53e2e35df4dd1a5dd15063a207a4
Markus Stange <markus.stange@gmail.com>
2023-10-22 20:03:05 -0400

    Use getNonInvertedCallNodeTable in flame graph code.
    Flame graph code is always executed with a non-inverted CallNodeInfo, so
    getNonInvertedCallNodeTable always returns the same as getCallNodeTable.
    
    The purpose of this patch is to remove more callers of getCallNodeTable()
    without changing behavior.

--- src/test/unit/profile-tree.test.js
+++ src/test/unit/profile-tree.test.js
@@ -127,7 +127,7 @@ describe('unfiltered call tree', function () {
       const cnKN = callNodeInfo.getCallNodeIndexFromPath([K, N]);
 
       const rows = computeFlameGraphRows(
-        callNodeInfo.getCallNodeTable(),
+        callNodeInfo.getNonInvertedCallNodeTable(),
         thread.funcTable,
         thread.stringTable
       );
