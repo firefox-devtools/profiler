0b195fd10eea1f02381221b794977c4ffbf267cb
Markus Stange <markus.stange@gmail.com>
2024-01-20 00:09:27 -0500

    Remove now-unused stack-to-inverted-call-node  mapping.

--- src/profile-logic/call-node-info.js
+++ src/profile-logic/call-node-info.js
@@ -32,11 +32,6 @@ export class CallNodeInfoImpl implements CallNodeInfo {
   // The non-inverted call node table, regardless of isInverted().
   _nonInvertedCallNodeTable: CallNodeTable;
 
-  // The mapping of stack index to corresponding call node index. This maps to
-  // either the inverted or the non-inverted call node table, depending on
-  // isInverted().
-  _stackIndexToCallNodeIndex: Int32Array;
-
   // The mapping of stack index to corresponding non-inverted call node index.
   // This always maps to the non-inverted call node table, regardless of
   // isInverted().
@@ -50,12 +45,10 @@ export class CallNodeInfoImpl implements CallNodeInfo {
   constructor(
     callNodeTable: CallNodeTable,
     nonInvertedCallNodeTable: CallNodeTable,
-    stackIndexToCallNodeIndex: Int32Array,
     stackIndexToNonInvertedCallNodeIndex: Int32Array
   ) {
     this._callNodeTable = callNodeTable;
     this._nonInvertedCallNodeTable = nonInvertedCallNodeTable;
-    this._stackIndexToCallNodeIndex = stackIndexToCallNodeIndex;
     this._stackIndexToNonInvertedCallNodeIndex =
       stackIndexToNonInvertedCallNodeIndex;
   }
@@ -72,10 +65,6 @@ export class CallNodeInfoImpl implements CallNodeInfo {
 
   getCallNodeTable(): CallNodeTable {
     return this._callNodeTable;
-  }
-
-  getStackIndexToCallNodeIndex(): Int32Array {
-    return this._stackIndexToCallNodeIndex;
   }
 
   getNonInvertedCallNodeTable(): CallNodeTable {
@@ -239,7 +228,6 @@ export class CallNodeInfoInvertedImpl
   constructor(
     callNodeTable: CallNodeTable,
     nonInvertedCallNodeTable: CallNodeTable,
-    stackIndexToCallNodeIndex: Int32Array,
     stackIndexToNonInvertedCallNodeIndex: Int32Array,
     suffixOrderedCallNodes: Uint32Array,
     suffixOrderIndexes: Uint32Array
@@ -247,7 +235,6 @@ export class CallNodeInfoInvertedImpl
     super(
       callNodeTable,
       nonInvertedCallNodeTable,
-      stackIndexToCallNodeIndex,
       stackIndexToNonInvertedCallNodeIndex
     );
     this._suffixOrderedCallNodes = suffixOrderedCallNodes;
