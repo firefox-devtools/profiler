9f1c8f311d03b4c451080b75e6a96a7a805bafe0
Markus Stange <markus.stange@gmail.com>
2024-01-20 00:00:33 -0500

    Implement getTreeOrderComparator for the inverted tree with non-inverted call nodes.
    This function is used when hovering or clicking the activity graph.
    
    This commit replaces a SampleIndexToCallNodeIndex mapping with a
    SampleIndexToNonInvertedCallNodeIndex mapping.

--- src/selectors/per-thread/stack-sample.js
+++ src/selectors/per-thread/stack-sample.js
@@ -299,7 +299,8 @@ export function getStackAndSampleSelectorsPerThread(
   const getTreeOrderComparatorInFilteredThread: Selector<
     (IndexIntoSamplesTable, IndexIntoSamplesTable) => number,
   > = createSelector(
-    getSampleIndexToCallNodeIndexForFilteredThread,
+    getSampleIndexToNonInvertedCallNodeIndexForFilteredThread,
+    getCallNodeInfo,
     ProfileData.getTreeOrderComparator
   );
 
