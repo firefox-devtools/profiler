17f70dba02b1f105e2686e5ba4435b5f9afe17ae
Richard Fine <richardf@unity3d.com>
2024-09-10 11:57:02 +0100

    Finish implementing symbolication tool

--- src/symbolicator.index.js
+++ src/symbolicator.index.js
@@ -1,34 +1,25 @@
-import {
-    unserializeProfileOfArbitraryFormat,
-} from './profile-logic/process-profile';
+var fs = require('fs');
 
+import { unserializeProfileOfArbitraryFormat } from './profile-logic/process-profile';
 import { SymbolStore } from './profile-logic/symbol-store';
-
 import {
     symbolicateProfile,
     applySymbolicationSteps
 } from './profile-logic/symbolication';
-
 import * as MozillaSymbolicationAPI from './profile-logic/mozilla-symbolication-api';
-
 import { SymbolsNotFoundError } from './profile-logic/errors';
 
 var argv = require('minimist')(process.argv.slice(2));
-if (!('profile' in argv))
+if (!('input' in argv 
+  && 'output' in argv 
+  && 'server' in argv))
 {
-    console.error("--profile argument (path to profile file) must be supplied");
-    process.exit(1);
-}
-if (!('server' in argv))
-{
-    console.error("--server argument (URL of symbol server) must be supplied");
-    process.exit(1);
+  console.error("Missing mandatory argument. Usage: symbolicator.js --input <path/to/input.json> --output <path/to/output.json> --server <URI of symbolication server>");
+  process.exit(1);
 }
 
-var fs = require('fs');
-var serializedProfile = JSON.parse(fs.readFileSync(argv.profile, 'utf8'));
-console.log(`Loaded profile from ${argv.profile}`);
-
+console.log(`Loading profile from ${argv.input}`);
+var serializedProfile = JSON.parse(fs.readFileSync(argv.input, 'utf8'));
 var profile = await unserializeProfileOfArbitraryFormat(serializedProfile);
 if (profile === undefined) {
     throw new Error('Unable to parse the profile.');
@@ -36,7 +27,9 @@ if (profile === undefined) {
 
 /**
  * Simple 'in-memory' symbol DB that conforms to the same interface as SymbolStoreDB but
- * just stores everything in a simple dictionary instead of IndexedDB
+ * just stores everything in a simple dictionary instead of IndexedDB. The composite key
+ * [debugName, breakpadId] is flattened to a string "debugName:breakpadId" to use as the
+ * map key.
  */
 class InMemorySymbolDB {
 
@@ -81,44 +74,34 @@ class InMemorySymbolDB {
 
 const symbolStoreDB = new InMemorySymbolDB();
 
-async function requestSymbolsWithCallback(
-    symbolSupplierName: string,
-    requests: LibSymbolicationRequest[],
-    callback: (path: string, requestJson: string) => Promise<MixedObject>
-  ) {
-    for (const { lib } of requests) {
-      console.log(`requestingSymbolTable(${lib.debugName})`);
-    }
-    try {
-      return await MozillaSymbolicationAPI.requestSymbols(
-        symbolSupplierName,
-        requests,
-        callback
-      );
-    } catch (e) {
-      throw new Error(
-        `There was a problem with the symbolication API request to the ${symbolSupplierName}: ${e.message}`
-      );
-    } finally {
-      for (const { lib } of requests) {
-        console.log(`receivedSymbolTableReply(${lib.debugName})`);
-      }
-    }
-  }
-
+/**
+ * SymbolStore implementation which just forwards everything to the symbol server in
+ * MozillaSymbolicationAPI format. No support for getting symbols from 'the browser' as
+ * there is no browser in this context.
+ */
 const symbolStore = new SymbolStore(symbolStoreDB, {
-    requestSymbolsFromServer: (requests) =>
-      requestSymbolsWithCallback(
-        'symbol server',
-        requests,
-        async (path, json) => {
-          const response = await fetch(argv.server + path, {
-            body: json,
-            method: 'POST'
-          });
-          return response.json();
-        }
-      ),
+    requestSymbolsFromServer: async (requests) => {
+      for (const { lib } of requests) {
+        console.log(`  Loading symbols for ${lib.debugName}`);
+      }
+      try {
+        return await MozillaSymbolicationAPI.requestSymbols(
+          'symbol server',
+          requests,
+          async (path, json) => {
+            const response = await fetch(argv.server + path, {
+              body: json,
+              method: 'POST'
+            });
+            return response.json();
+          }
+        );
+      } catch (e) {
+        throw new Error(
+          `There was a problem with the symbolication API request to the symbol server: ${e.message}`
+        );
+      }
+    },
 
     requestSymbolsFromBrowser: async (requests) => {
         return new Promise((resolve, reject) => { resolve([]); });
@@ -129,8 +112,9 @@ const symbolStore = new SymbolStore(symbolStoreDB, {
     },
   });
 
-const completionPromises = [];
+console.log("Symbolicating...");
 
+const symbolicationStepsPerThread : Map<ThreadIndex, SymbolicationStepInfo[]> = new Map();
 await symbolicateProfile(
     profile,
     symbolStore,
@@ -138,17 +122,29 @@ await symbolicateProfile(
         threadIndex: ThreadIndex,
         symbolicationStepInfo: SymbolicationStepInfo
       ) => {
-        completionPromises.push(
-          new Promise((resolve) => {
-            _symbolicationStepQueueSingleton.enqueueSingleSymbolicationStep(
-              dispatch,
-              threadIndex,
-              symbolicationStepInfo,
-              resolve
-            );
-          })
-        );
+        let threadSteps = symbolicationStepsPerThread.get(threadIndex);
+        if (threadSteps === undefined) {
+          threadSteps = [];
+          symbolicationStepsPerThread.set(threadIndex, threadSteps);
+        }
+        threadSteps.push(symbolicationStepInfo);
       }
 );
 
-await Promise.all(completionPromises);
+console.log("Applying collected symbolication steps...");
+
+profile.threads = profile.threads.map((oldThread, threadIndex) => {
+  const symbolicationSteps = symbolicationStepsPerThread.get(threadIndex);
+  if (symbolicationSteps === undefined) {
+    return oldThread;
+  }
+  const { thread, _ } = applySymbolicationSteps(oldThread, symbolicationSteps);
+  return thread;
+});
+
+profile.meta.symbolicated = true;
+
+console.log(`Saving profile to ${argv.output}`);
+fs.writeFileSync(argv.output, JSON.stringify(profile));
+console.log("Finished.");
+process.exit(0);
