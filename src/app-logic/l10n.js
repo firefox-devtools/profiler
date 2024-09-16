6b8d13c07dab4a1e14af21f56d95707f0e7bbd98
Julien Wajsberg <felash@gmail.com>
2024-09-16 11:19:54 +0200

    Enable the Friulian locale (#5127)

--- src/app-logic/l10n.js
+++ src/app-logic/l10n.js
@@ -29,6 +29,7 @@ export const AVAILABLE_LOCALES_TO_LOCALIZED_NAMES = {
   'es-CL': 'Español', // Use "Español (CL)" once we have more spanish versions
   fr: 'Français',
   'fy-NL': 'Frysk',
+  fur: 'Furlan',
   ia: 'Interlingua',
   it: 'Italiano',
   nl: 'Nederlands',
