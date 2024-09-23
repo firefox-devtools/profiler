91fe71ee5fe363b4280f57bdfdbe2127b78226fc
Julien Wajsberg <felash@gmail.com>
2024-09-23 17:02:30 +0200

    Export a tool to extract gecko logs from a profile (#4973)

--- src/test/unit/window-console.test.js
+++ src/test/unit/window-console.test.js
@@ -3,11 +3,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // @flow
+import { stripIndent } from 'common-tags';
+
 import {
   addDataToWindowObject,
   logFriendlyPreamble,
 } from '../../utils/window-console';
-import { storeWithSimpleProfile } from '../fixtures/stores';
+import { storeWithSimpleProfile, storeWithProfile } from '../fixtures/stores';
+import { getProfileWithMarkers } from '../fixtures/profiles/processed-profile';
 
 describe('console-accessible values on the window object', function () {
   // Coerce the window into a generic object, as these values aren't defined
@@ -36,5 +39,38 @@ describe('console-accessible values on the window object', function () {
     expect(console.log.mock.calls.length).toEqual(2);
     expect(console.log.mock.calls).toMatchSnapshot();
     (console: any).log = log;
+  });
+
+  it('can extract gecko logs', function () {
+    const profile = getProfileWithMarkers([
+      [
+        'LogMessages',
+        170,
+        null,
+        {
+          type: 'Log',
+          module: 'nsHttp',
+          name: 'ParentChannelListener::ParentChannelListener [this=7fb5e19b98d0, next=7fb5f48f2320]',
+        },
+      ],
+      [
+        'LogMessages',
+        190,
+        null,
+        {
+          type: 'Log',
+          name: 'nsJARChannel::nsJARChannel [this=0x87f1ec80]\n',
+          module: 'nsJarProtocol',
+        },
+      ],
+    ]);
+    const store = storeWithProfile(profile);
+    const target = {};
+    addDataToWindowObject(store.getState, store.dispatch, target);
+    const result = target.extractGeckoLogs();
+    expect(result).toBe(stripIndent`
+      1970-01-01 00:00:00.170000000 UTC - [Unknown Process 0: Empty]: D/nsHttp ParentChannelListener::ParentChannelListener [this=7fb5e19b98d0, next=7fb5f48f2320]
+      1970-01-01 00:00:00.190000000 UTC - [Unknown Process 0: Empty]: D/nsJarProtocol nsJARChannel::nsJARChannel [this=0x87f1ec80]
+    `);
   });
 });
