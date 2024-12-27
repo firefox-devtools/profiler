82272682ff1a30aa678a657cfc7360fc88368807
Markus Stange <markus.stange@gmail.com>
2024-12-27 11:31:16 +0100

    Remove getMarkerSchemaName and the now-unused marker name argument from getSchemaFromMarker.

--- src/profile-logic/marker-data.js
+++ src/profile-logic/marker-data.js
@@ -14,7 +14,6 @@ import {
   INTERVAL_END,
 } from 'firefox-profiler/app-logic/constants';
 import {
-  getMarkerSchemaName,
   getSchemaFromMarker,
   markerPayloadMatchesSearch,
 } from './marker-schema';
@@ -226,11 +225,7 @@ function positiveFilterMarker(
     }
 
     // Now check the schema for the marker payload for searchable
-    const markerSchema = getSchemaFromMarker(
-      markerSchemaByName,
-      marker.name,
-      marker.data
-    );
+    const markerSchema = getSchemaFromMarker(markerSchemaByName, marker.data);
     if (
       markerSchema &&
       markerPayloadMatchesSearch(markerSchema, marker, stringTable, test)
@@ -292,11 +287,7 @@ function negativeFilterMarker(
     }
 
     // Now check the schema for the marker payload for searchable
-    const markerSchema = getSchemaFromMarker(
-      markerSchemaByName,
-      marker.name,
-      marker.data
-    );
+    const markerSchema = getSchemaFromMarker(markerSchemaByName, marker.data);
 
     if (
       markerSchema &&
@@ -1287,14 +1278,12 @@ export function getAllowMarkersWithNoSchema(
     const { data } = marker;
 
     if (!data) {
-      // Keep the marker if there is payload.
+      // Keep the marker if there is no payload.
       return true;
     }
 
     if (!markerSchemaByName[data.type]) {
-      // Keep the marker if there is no schema. Note that this function doesn't use
-      // the getMarkerSchemaName function, as that function attempts to find a
-      // marker schema name in a very permissive manner. In the marker chart
+      // Keep the marker if there is no schema. In the marker chart
       // and marker table, most likely we want to show everything.
       return true;
     }
@@ -1528,11 +1517,7 @@ export function filterMarkerByDisplayLocation(
       return additionalResult;
     }
 
-    const schemaName = getMarkerSchemaName(
-      markerSchemaByName,
-      marker.name,
-      marker.data
-    );
+    const schemaName = marker.data ? (marker.data.type ?? null) : null;
     return schemaName !== null && markerTypes.has(schemaName);
   });
 }
--- src/selectors/per-thread/markers.js
+++ src/selectors/per-thread/markers.js
@@ -11,10 +11,7 @@ import * as MarkerData from '../../profile-logic/marker-data';
 import * as MarkerTimingLogic from '../../profile-logic/marker-timing';
 import * as ProfileSelectors from '../profile';
 import { getRightClickedMarkerInfo } from '../right-clicked-marker';
-import {
-  getLabelGetter,
-  getMarkerSchemaName,
-} from '../../profile-logic/marker-schema';
+import { getLabelGetter } from '../../profile-logic/marker-schema';
 import { getInclusiveSampleIndexRangeForSelection } from '../../profile-logic/profile-data';
 
 import type { BasicThreadSelectorsPerThread } from './thread';
@@ -682,11 +679,7 @@ export function getMarkerSelectorsPerThread(
             if (
               data &&
               marker.name === name &&
-              getMarkerSchemaName(
-                ProfileSelectors.getMarkerSchemaByName,
-                marker.name,
-                data
-              ) === schemaName &&
+              data.type === schemaName &&
               keys.every((key) => key in data)
             ) {
               markerIndexes.push(index);
