devtools-html/cleopatra

WIP traced value support

src/types/profile.js
@@ -118,6 +118,7 @@ export type RawSamplesTable = {|
   time?: Milliseconds[],
   // If the `time` column is not present, then the `timeDeltas` column must be present.
   timeDeltas?: Milliseconds[],
+  argv?: Array<number | null>,
   // An optional weight array. If not present, then the weight is assumed to be 1.
   // See the WeightType type for more information.
   weight: null | number[],
@@ -133,6 +134,7 @@ export type RawSamplesTable = {|
   // This property isn't present in normal threads. However it's present for
   // merged threads, so that we know the origin thread for these samples.
   threadId?: Tid[],
+  argv?: Array<number | null>,
   length: number,
 |};
 
@@ -636,7 +638,9 @@ export type RawThread = {|
   // It's absent in Firefox 97 and before, or in Firefox 98+ when this thread
   // had no extra attribute at all.
   userContextId?: number,
-|};
+  argvBuffer?: ArrayBuffer,
+  shapes?: Array<?Array<string>>,
+  |};
 
 export type ExtensionTable = {|
   baseURL: string[],

firefox-devtools/profiler

src/types/profile.js
@@ -118,6 +118,7 @@ export type RawSamplesTable = {|
   time?: Milliseconds[],
   // If the `time` column is not present, then the `timeDeltas` column must be present.
   timeDeltas?: Milliseconds[],
+  argv?: Array<number | null>,
   // An optional weight array. If not present, then the weight is assumed to be 1.
   // See the WeightType type for more information.
   weight: null | number[],
@@ -133,6 +134,7 @@ export type RawSamplesTable = {|
   // This property isn't present in normal threads. However it's present for
   // merged threads, so that we know the origin thread for these samples.
   threadId?: Tid[],
+  argv?: Array<number | null>,
   length: number,
 |};
 
@@ -165,6 +167,7 @@ export type UnbalancedNativeAllocationsTable = {|
   weight: Bytes[],
   weightType: 'bytes',
   stack: Array<IndexIntoStackTable | null>,
+  argv?: Array<number | null>,
   length: number,
 |};
 
@@ -175,7 +178,7 @@ export type BalancedNativeAllocationsTable = {|
   ...UnbalancedNativeAllocationsTable,
   memoryAddress: number[],
   threadId: number[],
-|};
+  |};
 
 /**
  * Native allocations are recorded as a marker payload, but in profile processing they
@@ -505,6 +508,7 @@ export type RawCounterSamplesTable = {|
   number?: number[],
   // The count of the data, for instance for memory this would be bytes.
   count: number[],
+  argv?: Array<number | null>,
   length: number,
 |};
 
@@ -676,7 +680,9 @@ export type RawThread = {|
   // It's absent in Firefox 97 and before, or in Firefox 98+ when this thread
   // had no extra attribute at all.
   userContextId?: number,
-|};
+  argvBuffer?: string,
+  shapes?: Array<?Array<string>>,
+  |};
 
 export type ExtensionTable = {|
   baseURL: string[],