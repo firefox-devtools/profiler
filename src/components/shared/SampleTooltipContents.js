devtools-html/cleopatra

Show size units in the timeline for profiles where the first thread's weightType is bytes.

src/components/shared/SampleTooltipContents.js
@@ -12,7 +12,7 @@ import {
   getFuncNamesAndOriginsForPath,
   convertStackToCallNodeAndCategoryPath,
 } from 'firefox-profiler/profile-logic/profile-data';
-import { getFormattedTimeLength } from 'firefox-profiler/profile-logic/committed-ranges';
+import { getFormattedLength } from 'firefox-profiler/profile-logic/committed-ranges';
 import {
   formatMilliseconds,
   formatPercent,
@@ -42,6 +42,7 @@ type Props = {|
   +cpuRatioInTimeRange: CPUProps | null,
   +sampleIndex: IndexIntoSamplesTable | null,
   +zeroAt: Milliseconds,
+  +profileTimelineUnit: string,
   +interval: Milliseconds,
 |};
 
@@ -126,6 +127,7 @@ export class SampleTooltipContents extends React.PureComponent<Props> {
       categories,
       implementationFilter,
       zeroAt,
+      profileTimelineUnit,
       interval,
     } = this.props;
 
@@ -148,8 +150,9 @@ export class SampleTooltipContents extends React.PureComponent<Props> {
         hasStack = stack.length > 1 || stack[0].funcName !== '(root)';
       }
 
-      formattedSampleTime = getFormattedTimeLength(
+      formattedSampleTime = getFormattedLength(
         sampleTime - zeroAt,
+        profileTimelineUnit,
         // Make sure that we show enough precision for the given sample interval.
         interval / 10
       );

src/profile-logic/committed-ranges.js
@@ -3,7 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
 
-import { formatTimestamp } from 'firefox-profiler/utils/format-numbers';
+import {
+  formatBytes,
+  formatTimestamp,
+} from 'firefox-profiler/utils/format-numbers';
 import type { Milliseconds, StartEndRange } from 'firefox-profiler/types';
 
 /**
@@ -164,10 +167,19 @@ export function stringifyCommittedRanges(
   return arrayValue.map(stringifyStartEnd).join('~');
 }
 
-export function getFormattedTimeLength(
+export function getFormattedLength(
   length: Milliseconds,
+  unit: string,
   precision: Milliseconds = Infinity
 ): string {
+  if (unit === 'bytes') {
+    return formatBytes(
+      length,
+      /*significantdigits*/ 2,
+      /*maxFractionalDigits*/ 2,
+      precision
+    );
+  }
   return formatTimestamp(
     length,
     /*significantdigits*/ 2,
@@ -177,10 +189,11 @@ export function getFormattedTimeLength(
 }
 
 export function getCommittedRangeLabels(
-  committedRanges: StartEndRange[]
+  committedRanges: StartEndRange[],
+  unit: string
 ): string[] {
   const labels = committedRanges.map((range) =>
-    getFormattedTimeLength(range.end - range.start)
+    getFormattedLength(range.end - range.start, unit)
   );
   return labels;
 }

firefox-devtools/profiler

src/components/shared/SampleTooltipContents.js
@@ -12,7 +12,7 @@ import {
   getFuncNamesAndOriginsForPath,
   convertStackToCallNodeAndCategoryPath,
 } from 'firefox-profiler/profile-logic/profile-data';
-import { getFormattedTimeLength } from 'firefox-profiler/profile-logic/committed-ranges';
+import { getFormattedTimelineValue } from 'firefox-profiler/profile-logic/committed-ranges';
 import {
   formatMilliseconds,
   formatPercent,
@@ -42,6 +42,7 @@ type Props = {|
   +cpuRatioInTimeRange: CPUProps | null,
   +sampleIndex: IndexIntoSamplesTable | null,
   +zeroAt: Milliseconds,
+  +profileTimelineUnit: string,
   +interval: Milliseconds,
 |};
 
@@ -126,6 +127,7 @@ export class SampleTooltipContents extends React.PureComponent<Props> {
       categories,
       implementationFilter,
       zeroAt,
+      profileTimelineUnit,
       interval,
     } = this.props;
 
@@ -148,8 +150,9 @@ export class SampleTooltipContents extends React.PureComponent<Props> {
         hasStack = stack.length > 1 || stack[0].funcName !== '(root)';
       }
 
-      formattedSampleTime = getFormattedTimeLength(
+      formattedSampleTime = getFormattedTimelineValue(
         sampleTime - zeroAt,
+        profileTimelineUnit,
         // Make sure that we show enough precision for the given sample interval.
         interval / 10
       );

src/profile-logic/committed-ranges.js
@@ -3,7 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
 
-import { formatTimestamp } from 'firefox-profiler/utils/format-numbers';
+import {
+  formatBytes,
+  formatTimestamp,
+} from 'firefox-profiler/utils/format-numbers';
 import type { Milliseconds, StartEndRange } from 'firefox-profiler/types';
 
 /**
@@ -164,23 +167,33 @@ export function stringifyCommittedRanges(
   return arrayValue.map(stringifyStartEnd).join('~');
 }
 
-export function getFormattedTimeLength(
+export function getFormattedTimelineValue(
   length: Milliseconds,
+  unit: string,
   precision: Milliseconds = Infinity
 ): string {
+  if (unit === 'bytes') {
+    return formatBytes(
+      length,
+      /*significantDigits*/ 2,
+      /*maxFractionalDigits*/ 2,
+      precision
+    );
+  }
   return formatTimestamp(
     length,
-    /*significantdigits*/ 2,
+    /*significantDigits*/ 2,
     /*maxFractionalDigits*/ 2,
     precision
   );
 }
 
 export function getCommittedRangeLabels(
-  committedRanges: StartEndRange[]
+  committedRanges: StartEndRange[],
+  unit: string
 ): string[] {
   const labels = committedRanges.map((range) =>
-    getFormattedTimeLength(range.end - range.start)
+    getFormattedTimelineValue(range.end - range.start, unit)
   );
   return labels;
 }