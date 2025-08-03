7657b82bcb1380e5041ad2107ab4a138c518e113
Markus Stange <mstange.moz@gmail.com>
2025-08-03 17:43:36 -0400

    Fix IDBDatabase type.

--- src/profile-logic/symbol-store-db.ts
+++ src/profile-logic/symbol-store-db.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { IDBDatabase, IDBObjectStore } from 'firefox-profiler/types';
 import { SymbolsNotFoundError } from './errors';
 
 // Contains a symbol table, which can be used to map addresses to strings.
@@ -40,7 +41,7 @@ type SymbolItem = {
 
 type SymbolPrimaryKey = [string, string];
 type SymbolDateKey = SymbolItem['lastUsedDate'];
-type SymbolStore = IDBObjectStore;
+type SymbolStore = IDBObjectStore<SymbolPrimaryKey, SymbolItem>;
 
 const kTwoWeeksInMilliseconds = 2 * 7 * 24 * 60 * 60 * 1000;
 
