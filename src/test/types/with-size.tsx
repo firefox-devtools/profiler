da6a37ab9945a5281d6563a0e907d887b92e70c8
Markus Stange <mstange.moz@gmail.com>
2025-08-04 12:16:59 -0400

    Port withSize test.

--- src/test/types/with-size.tsx
+++ src/test/types/with-size.tsx
@@ -5,27 +5,28 @@
 
 import * as React from 'react';
 import { withSize, type SizeProps } from '../../components/shared/WithSize';
-/* eslint-disable no-unused-vars */
 
 /**
  * This file tests the WithSize higher order component, and makes sure that it correctly
- * strips off the width and height from the component's props.
+ * catches mismatches of props with the type system.
  */
 
 type Props = {
-  +ownPropA: 'a',
-  +ownPropB: 'b',
-  ...SizeProps,
-};
+  readonly ownPropA: 'a';
+  readonly ownPropB: 'b';
+} & SizeProps;
+
+function expectType<T>(_a: T) {}
+function markUsed<T>(_a: T) {}
 
 class ExampleComponent extends React.PureComponent<Props> {
   render() {
-    (this.props.ownPropA: 'a');
-    (this.props.ownPropB: 'b');
-    // $FlowExpectError  - There is no "ownPropC"
-    (this.props.ownPropC: 'c');
-    (this.props.width: number);
-    (this.props.height: number);
+    expectType<'a'>(this.props.ownPropA);
+    expectType<'b'>(this.props.ownPropB);
+    // @ts-expect-error  - There is no "ownPropC"
+    expectType<any>(this.props.ownPropC);
+    expectType<number>(this.props.width);
+    expectType<number>(this.props.height);
 
     return null;
   }
@@ -35,41 +36,47 @@ class ExampleComponent extends React.PureComponent<Props> {
 const example1 = (
   <ExampleComponent ownPropA="a" ownPropB="b" width={10} height={10} />
 );
+markUsed(example1);
 
 // Passing in an unknown prop triggers an error.
 const example2 = (
-  // $FlowExpectError - There is no "ownPropC"
   <ExampleComponent
     ownPropA="a"
     ownPropB="b"
+    // @ts-expect-error - There is no "ownPropC"
     ownPropC="c"
     width={10}
     height={10}
   />
 );
+markUsed(example2);
 
-const ExampleComponentWithSize = withSize<Props>(ExampleComponent);
+const ExampleComponentWithSize = withSize(ExampleComponent);
 
 // This it the correct use
 const exampleWithSize1 = <ExampleComponentWithSize ownPropA="a" ownPropB="b" />;
+markUsed(exampleWithSize1);
 
 const exampleWithSize2 = (
-  // $FlowExpectError - The width and height are already provided.
+  // @ts-expect-error - The width and height are already provided.
   <ExampleComponentWithSize ownPropA="a" ownPropB="b" width={10} height={10} />
 );
+markUsed(exampleWithSize2);
 
 // Passing in an unknown prop triggers an error.
 const exampleWithSize3 = (
-  // $FlowExpectError - The width and height are already provided.
+  // @ts-expect-error - The width and height are already provided.
   <ExampleComponentWithSize ownPropA="a" ownPropB="b" ownPropC="c" />
 );
+markUsed(exampleWithSize3);
 
-// $FlowExpectError - ownPropB was not passed in.
+// @ts-expect-error - ownPropB was not passed in.
 const exampleWithSize4 = <ExampleComponentWithSize ownPropA="a" />;
+markUsed(exampleWithSize4);
 
 type NoSizingProps = {
-  +ownPropA: 'a',
-  +ownPropB: 'b',
+  readonly ownPropA: 'a';
+  readonly ownPropB: 'b';
   // The size props are omitted.
 };
 
@@ -79,5 +86,6 @@ class NoSizing extends React.PureComponent<NoSizingProps> {
   }
 }
 
-// $FlowExpectError - The component does not have sizing props.
-const exampleNoSizing = withSize<Props>(NoSizing);
+// @ts-expect-error - The component does not have sizing props.
+const exampleNoSizing = withSize(NoSizing);
+markUsed(exampleNoSizing);
