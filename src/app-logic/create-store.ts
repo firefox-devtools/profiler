a2e42db6f25ea37cc9b5ed90c91d778711b8fef3
Markus Stange <mstange.moz@gmail.com>
2025-08-07 22:19:40 -0400

    Convert app-logic/create-store.js.

---
+++ src/app-logic/create-store.ts
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { createStore, applyMiddleware, type Middleware } from 'redux';
+import { thunk, type ThunkMiddleware } from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducers from 'firefox-profiler/reducers';
+import type { Action, State, Store } from 'firefox-profiler/types';
+
+/**
+ * Isolate the store creation into a function, so that it can be used outside of the
+ * app's execution context, e.g. for testing.
+ * @return {object} Redux store.
+ */
+export default function initializeStore(): Store {
+  let loggerMiddleware: Middleware<Action> | null = null;
+  if (process.env.NODE_ENV === 'development') {
+    loggerMiddleware = createLogger({
+      collapsed: true,
+      titleFormatter: (action, time, duration) =>
+        `[action]    ${action.type} (in ${duration.toFixed(2)} ms)`,
+      logErrors: false,
+      duration: true,
+    });
+  }
+
+  const thunkMiddleware: ThunkMiddleware<State, Action> = thunk;
+  const enhancer = loggerMiddleware
+    ? applyMiddleware(thunkMiddleware, loggerMiddleware)
+    : applyMiddleware(thunkMiddleware);
+  return createStore<State, Action>(reducers, enhancer);
+}
