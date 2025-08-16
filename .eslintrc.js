e179ac8bafc1c76cbf126a34d6778a1da70c1071
Nazım Can Altınova <canaltinova@gmail.com>
2025-08-16 21:39:16 +0300

    Merge multiple .eslintrc.js files into one
    This is going to help us upgrade the eslint version to 9.

--- .eslintrc.js
+++ .eslintrc.js
@@ -144,4 +144,67 @@ module.exports = {
   globals: {
     AVAILABLE_STAGING_LOCALES: true,
   },
+  overrides: [
+    {
+      files: ['src/test/**/*'],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest', 'testing-library', 'jest-formatting', 'jest-dom'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:testing-library/react',
+        'plugin:jest-dom/recommended',
+      ],
+      rules: {
+        'react/jsx-no-bind': 0,
+        // This rule isn't useful because use Flow.
+        'jest/valid-title': 0,
+
+        // Allow require(), for example for importing JSON files.
+        '@typescript-eslint/no-require-imports': 'off',
+
+        // Override the project-wide config to allow @ts-nocheck.
+        // We really just need this for our mocks.
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            // Allow @ts-expect-error and @ts-no-check annotations with descriptions.
+            'ts-expect-error': 'allow-with-description',
+            'ts-nocheck': 'allow-with-description',
+            // Don't allow @ts-ignore because we want to be notified
+            // when the error goes away so we can remove the annotation - use
+            // @ts-expect-error instead
+            'ts-ignore': true,
+            'ts-check': false,
+          },
+        ],
+
+        // Adding more errors now
+        'testing-library/no-manual-cleanup': 'error',
+        'testing-library/no-wait-for-snapshot': 'error',
+        'testing-library/prefer-explicit-assert': [
+          'error',
+          { includeFindQueries: false },
+        ],
+        'testing-library/prefer-presence-queries': 'error',
+
+        // Disable some rules that are in the "recommended" part.
+        // This is a purely stylistic rule
+        'testing-library/render-result-naming-convention': 'off',
+        // This disallows using `container`, but this is still useful for us sometimes
+        'testing-library/no-container': 'off',
+        // This disallows using direct Node properties (eg: firstChild), but we have
+        // legitimate uses:
+        'testing-library/no-node-access': 'off',
+        // Disable until https://github.com/testing-library/eslint-plugin-testing-library/issues/359
+        // is fixed.
+        'testing-library/await-async-query': 'off',
+
+        // Individual jest-formatting rules so that we format only test and describe blocks
+        'jest-formatting/padding-around-describe-blocks': 2,
+        'jest-formatting/padding-around-test-blocks': 2,
+      },
+    },
+  ],
 };
