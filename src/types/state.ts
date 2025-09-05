a085575d17760158865ed130c48c25baebdeba8f
Ryan Hunt <rhunt@eqrion.net>
2025-09-05 14:15:27 -0500

    Add a fullscreen button to the bottom box

--- src/types/state.ts
+++ src/types/state.ts
@@ -354,6 +354,7 @@ export type ProfileSpecificUrlState = {
   sourceView: SourceViewState;
   assemblyView: AssemblyViewState;
   isBottomBoxOpenPerPanel: IsOpenPerPanelState;
+  isBottomBoxFullscreen: boolean;
   globalTrackOrder: TrackIndex[];
   hiddenGlobalTracks: Set<TrackIndex>;
   hiddenLocalTracksByPid: Map<Pid, Set<TrackIndex>>;
