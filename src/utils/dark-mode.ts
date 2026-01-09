c0cae9871f23b90ceab8092534fd987b14e3c7f9
Tooru Fujisawa <arai_a@mac.com>
2026-01-10 02:04:41 +0900

    Apply dark mode to timeline markers

---
+++ src/utils/dark-mode.ts
@@ -0,0 +1,39 @@
+let _isDarkModeSetup = false;
+let _isDarkMode = false;
+
+export function isDarkMode() {
+  if (!_isDarkModeSetup) {
+    try {
+      function readSetting() {
+        const theme = window.localStorage.getItem('theme');
+        if (theme === 'dark') {
+          _isDarkMode = true;
+        } else {
+          _isDarkMode = false;
+        }
+      }
+      readSetting();
+      window.addEventListener('storage', (event: StorageEvent) => {
+        if (event.key === 'theme') {
+          readSetting();
+        }
+      });
+    } catch (e) {
+      console.warn('localStorage access denied', e);
+    }
+    _isDarkModeSetup = true;
+  }
+
+  return _isDarkMode;
+}
+
+export function lightDark(light: string, dark: string): string {
+  return isDarkMode() ? dark : light;
+}
+
+export function maybeLightDark(value: string | [string, string]): string {
+  if (typeof value === 'string') {
+    return value;
+  }
+  return lightDark(value[0], value[1]);
+}
