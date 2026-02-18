e09282ca25238fb796463a800c6f5526e8b6412c
fatadel <afatkhutdinov@mozilla.com>
2026-02-18 15:37:02 +0100

    incorporate feedback

--- src/selectors/url-state.ts
+++ src/selectors/url-state.ts
@@ -88,7 +88,7 @@ export const getAssemblyViewNativeSymbol: Selector<NativeSymbolInfo | null> = (
   const { nativeSymbols, currentNativeSymbol } =
     getProfileSpecificState(state).assemblyView;
   return currentNativeSymbol !== null
-    ? (nativeSymbols[currentNativeSymbol] ?? null)
+    ? nativeSymbols[currentNativeSymbol]
     : null;
 };
 export const getAssemblyViewCurrentNativeSymbolEntryIndex: Selector<
