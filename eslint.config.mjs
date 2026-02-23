28300c62d4ba4ea90c879f9e15dace94aa19ed21
Nazım Can Altınova <canaltinova@gmail.com>
2026-02-23 17:30:58 +0100

    Ignore the cli dist directory in eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig(
     ignores: [
       'src/profile-logic/import/proto/**',
       'src/types/libdef/npm/**',
+      'src/profile-query-cli/dist/**',
       'res/**',
       'dist/**',
       'docs-user/**',
