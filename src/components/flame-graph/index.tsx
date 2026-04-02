d6c804dc4b6bbbc85dbddff6a8177629aef5c6bb
Markus Stange <mstange.moz@gmail.com>
2026-04-02 15:19:01 -0400

    First attempt at FlameGraph refactor

--- src/components/flame-graph/index.tsx
+++ src/components/flame-graph/index.tsx
@@ -2,21 +2,65 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import * as React from 'react';
+
 import { StackSettings } from '../shared/StackSettings';
 import { TransformNavigator } from '../shared/TransformNavigator';
-import { MaybeFlameGraph } from './MaybeFlameGraph';
+import {
+  ConnectedFlameGraph,
+  type ConnectedFlameGraphHandle,
+} from './ConnectedFlameGraph';
+import { FlameGraphEmptyReasons } from './FlameGraphEmptyReasons';
+import explicitConnect from 'firefox-profiler/utils/connect';
+import { selectedThreadSelectors } from '../../selectors/per-thread';
 
-const FlameGraphView = () => (
-  <div
-    className="flameGraph"
-    id="flame-graph-tab"
-    role="tabpanel"
-    aria-labelledby="flame-graph-tab-button"
-  >
-    <StackSettings hideInvertCallstack={true} />
-    <TransformNavigator />
-    <MaybeFlameGraph />
-  </div>
-);
+import type { ConnectedProps } from 'firefox-profiler/utils/connect';
 
-export const FlameGraph = FlameGraphView;
+import './MaybeFlameGraph.css';
+
+type StateProps = {
+  readonly isPreviewSelectionEmpty: boolean;
+};
+
+type Props = ConnectedProps<{}, StateProps, {}>;
+
+class FlameGraphViewImpl extends React.PureComponent<Props> {
+  _connectedFlameGraph: React.RefObject<ConnectedFlameGraphHandle> =
+    React.createRef();
+
+  override componentDidMount() {
+    this._connectedFlameGraph.current?.focus();
+  }
+
+  override render() {
+    const { isPreviewSelectionEmpty } = this.props;
+
+    return (
+      <div
+        className="flameGraph"
+        id="flame-graph-tab"
+        role="tabpanel"
+        aria-labelledby="flame-graph-tab-button"
+      >
+        <StackSettings hideInvertCallstack={true} />
+        <TransformNavigator />
+        {isPreviewSelectionEmpty ? (
+          <FlameGraphEmptyReasons />
+        ) : (
+          <ConnectedFlameGraph ref={this._connectedFlameGraph} />
+        )}
+      </div>
+    );
+  }
+}
+
+const FlameGraphViewConnected = explicitConnect<{}, StateProps, {}>({
+  mapStateToProps: (state) => ({
+    isPreviewSelectionEmpty:
+      !selectedThreadSelectors.getHasPreviewFilteredCtssSamples(state),
+  }),
+  mapDispatchToProps: {},
+  component: FlameGraphViewImpl,
+});
+
+export const FlameGraph = FlameGraphViewConnected;
