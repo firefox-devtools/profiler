mstange/perf.html

Allow raw native symbol table columns to be typed arrays.

src/profile-logic/profile-data.ts
@@ -59,6 +59,7 @@ import type {
   FrameTable,
   FuncTable,
   NativeSymbolTable,
+  RawNativeSymbolTable,
   ResourceTable,
   CategoryList,
   IndexIntoCategoryList,
@@ -114,6 +115,7 @@ import type { CallNodeInfo, SuffixOrderIndex } from './call-node-info';
 import {
   toFloat64Array,
   toInt32Array,
+  toInt32ArraySetNullToNegOne,
   toUint8Array,
 } from 'firefox-profiler/utils/typed-arrays';
 import { bytesToBase64 } from 'firefox-profiler/utils/base64';
@@ -4617,20 +4619,22 @@ export function getNativeSymbolInfo(
   frameTable: FrameTable,
   stringTable: StringTable
 ): NativeSymbolInfo {
-  const functionSizeOrNull = nativeSymbols.functionSize[nativeSymbol];
-  const functionSize =
-    functionSizeOrNull ??
-    calculateFunctionSizeLowerBound(
-      frameTable,
-      nativeSymbols.address[nativeSymbol],
-      nativeSymbol
-    );
+  // `-1` is the sentinel for "size unknown" in the derived table.
+  const rawFunctionSize = nativeSymbols.functionSize[nativeSymbol];
+  const functionSizeIsKnown = rawFunctionSize !== -1;
+  const functionSize = functionSizeIsKnown
+    ? rawFunctionSize
+    : calculateFunctionSizeLowerBound(
+        frameTable,
+        nativeSymbols.address[nativeSymbol],
+        nativeSymbol
+      );
   return {
     libIndex: nativeSymbols.libIndex[nativeSymbol],
     address: nativeSymbols.address[nativeSymbol],
     name: stringTable.getString(nativeSymbols.name[nativeSymbol]),
     functionSize,
-    functionSizeIsKnown: functionSizeOrNull !== null,
+    functionSizeIsKnown,
   };
 }
 
@@ -4776,6 +4780,18 @@ export function computeFrameTableFromRawFrameTable(
     column: toInt32Array(rawFrameTable.column),
     originalLocation: toInt32Array(rawFrameTable.originalLocation),
     length: rawFrameTable.length,
+  };
+}
+
+export function computeNativeSymbolTableFromRawNativeSymbolTable(
+  raw: RawNativeSymbolTable
+): NativeSymbolTable {
+  return {
+    libIndex: toInt32Array(raw.libIndex),
+    address: toInt32Array(raw.address),
+    name: toInt32Array(raw.name),
+    functionSize: toInt32ArraySetNullToNegOne(raw.functionSize),
+    length: raw.length,
   };
 }
 

firefox-devtools/profiler

src/profile-logic/profile-data.ts
@@ -59,6 +59,7 @@ import type {
   FrameTable,
   FuncTable,
   NativeSymbolTable,
+  RawNativeSymbolTable,
   ResourceTable,
   CategoryList,
   IndexIntoCategoryList,
@@ -4751,20 +4752,22 @@ export function getNativeSymbolInfo(
   frameTable: FrameTable,
   stringTable: StringTable
 ): NativeSymbolInfo {
-  const functionSizeOrNull = nativeSymbols.functionSize[nativeSymbol];
-  const functionSize =
-    functionSizeOrNull ??
-    calculateFunctionSizeLowerBound(
-      frameTable,
-      nativeSymbols.address[nativeSymbol],
-      nativeSymbol
-    );
+  // `-1` is the sentinel for "size unknown" in the derived table.
+  const rawFunctionSize = nativeSymbols.functionSize[nativeSymbol];
+  const functionSizeIsKnown = rawFunctionSize !== -1;
+  const functionSize = functionSizeIsKnown
+    ? rawFunctionSize
+    : calculateFunctionSizeLowerBound(
+        frameTable,
+        nativeSymbols.address[nativeSymbol],
+        nativeSymbol
+      );
   return {
     libIndex: nativeSymbols.libIndex[nativeSymbol],
     address: nativeSymbols.address[nativeSymbol],
     name: stringTable.getString(nativeSymbols.name[nativeSymbol]),
     functionSize,
-    functionSizeIsKnown: functionSizeOrNull !== null,
+    functionSizeIsKnown,
   };
 }
 
@@ -4929,6 +4932,18 @@ export function computeFrameTableFromRawFrameTable(
     column: toInt32Array(rawFrameTable.column),
     originalLocation: toInt32Array(rawFrameTable.originalLocation),
     length: rawFrameTable.length,
+  };
+}
+
+export function computeNativeSymbolTableFromRawNativeSymbolTable(
+  raw: RawNativeSymbolTable
+): NativeSymbolTable {
+  return {
+    libIndex: toInt32Array(raw.libIndex),
+    address: toUint32Array(raw.address),
+    name: toInt32Array(raw.name),
+    functionSize: toInt32Array(raw.functionSize),
+    length: raw.length,
   };
 }
 